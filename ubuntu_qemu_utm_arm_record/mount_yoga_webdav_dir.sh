ab0d2708928ab6a7b02222f9a36babe1738e728a
jamesbrown <randomvoidmail@foxmail.com>
2023-05-15 15:54:00 +0800

    update

--- ubuntu_qemu_utm_arm_record/mount_yoga_webdav_dir.sh
+++ ubuntu_qemu_utm_arm_record/mount_yoga_webdav_dir.sh
@@ -1,6 +1,12 @@
 cd agi_computer_control/ubuntu_qemu_utm_arm_record
 
+# enable sudo
 echo -e "KSDA37287522\n" | sudo -S echo "enable sudo"
+
+# remove davfs pid locks
+sudo rm /var/run/mount.davfs/mnt-dav1.pid
+sudo rm /var/run/mount.davfs/mnt-dav2.pid
+
 sudo apt install -y davfs2
 #sudo mkdir /mnt/dav1
 sudo mkdir /mnt/dav2
@@ -16,7 +22,6 @@ echo -e "root\nroot\n" | sudo mount -t davfs -o noexec http://192.168.56.1:8111 
 
 #mkdir scripts
 #mkdir recordings
-
 #ln -s /mnt/dav1 scripts
 ln -s /mnt/dav2 recordings
 
