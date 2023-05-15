f2e68a18f6307014c1e97bf743795ea18dfdf49c
jamesbrown <randomvoidmail@foxmail.com>
2023-05-15 15:16:00 +0800

    update

---
+++ ubuntu_qemu_utm_arm_record/launch_server.cmd
@@ -0,0 +1,1 @@
+webdav-cli --host 192.168.56.1 --port 8111 --username root --password root --path=D:\works\agi_computer_recordings\recordings 
---
+++ ubuntu_qemu_utm_arm_record/mount_yoga_webdav_dir.sh
@@ -0,0 +1,24 @@
+cd agi_computer_control/ubuntu_qemu_utm_arm_record
+
+echo -e "KSDA37287522\n" | sudo -S echo "enable sudo"
+sudo apt install -y davfs2
+#sudo mkdir /mnt/dav1
+sudo mkdir /mnt/dav2
+
+sudo umount /mnt/dav1
+sudo umount /mnt/dav2
+
+sudo rm -rf scripts
+sudo rm -rf recordings
+
+#echo -e "root\nroot\n" | sudo mount -t davfs -o noexec http://192.168.56.1:8110 /mnt/dav1
+echo -e "root\nroot\n" | sudo mount -t davfs -o noexec http://192.168.56.1:8111 /mnt/dav2
+
+#mkdir scripts
+#mkdir recordings
+
+#ln -s /mnt/dav1 scripts
+ln -s /mnt/dav2 recordings
+
+
+
