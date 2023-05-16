8b2dd67c1f41761ef74b007b2478768b2b4598bb
GitJournal <app@gitjournal.io>
2023-05-17 01:42:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
+++ ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
@@ -2,4 +2,7 @@
 # use tmux automation tool, reading yaml config and create multiple windows.
 tmux kill-session -t kali_prepare_two_webdav_dirs
 # tmuxp load kali_prepare_two_webdav_dirs.yaml
-gnome-terminal -- tmuxp load kali_prepare_two_webdav_dirs.yaml
+
+# use absolute path.
+
+gnome-terminal -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml"
