b870df2941884ab8409ac33524ac1347337fddbe
GitJournal <app@gitjournal.io>
2023-05-17 07:10:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/duplicate_frame_detector.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/duplicate_frame_detector.py
@@ -4,4 +4,5 @@ screenshot_vm = "screenshot_vm.sh"
 image_path = "output.png"
 exit_code = os.system(screenshot_vm)
 
+lock_indicator_path = "lock_indicator.png"
 # right now just check the login screen. if it hits the login screen we reboot.
