ad60d34e41d403db43666e3498b40068175939af
GitJournal <app@gitjournal.io>
2023-05-17 00:58:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
@@ -1,4 +1,14 @@
 import time
-
+import os
 restore_vm = "restore_vm.sh"
+stop_vm = "stop_vm.sh"
+start_vm = "start_vm.sh"
+mount_kali_webdav_dirs = "../mount_kali_webdav_dirs.sh"
 
+def run_bash_script(script_path):
+    os.system("")
+while True:
+    os.system(stop_vm)
+    os.system()
+    os.system(restore_vm)
+    os.system(start_vm)
