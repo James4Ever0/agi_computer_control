e12264a5b484a16ba965c43d5897559151fa691f
jamesbrown <randomvoidmail@foxmail.com>
2023-05-16 14:32:00 +0800

    update

--- ubuntu_qemu_utm_arm_record/macos_iterate_windows.sh
+++ ubuntu_qemu_utm_arm_record/macos_iterate_windows.sh
@@ -1,3 +1,5 @@
 python3 -c "from screenshot import get_window_id; print(get_window_id.get_window_info(options=get_window_id.build_option_bitmask()))" | grep Name | less
 
 # or use obs, then use obs-websocket to take screeenshots.
+
+# enable screen recording capability with iterm.
