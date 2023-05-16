1dbc5e821120345789e1f8ef4514813d9cc9939f
GitJournal <app@gitjournal.io>
2023-05-17 01:10:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
@@ -8,10 +8,13 @@ stop_vm = "stop_vm.sh"
 start_vm = "start_vm.sh"
 mount_kali_webdav_dirs = "../mount_kali_webdav_dirs.sh"
 
+
 def run_bash_script(script_path):
     print("Excuting bash script: %s" % script_path)
     command = f"bash {script_path}"
     os.system(command)
+
+
 while True:
     run_bash_script(stop_vm)
     run_bash_script(mount_kali_webdav_dirs)
