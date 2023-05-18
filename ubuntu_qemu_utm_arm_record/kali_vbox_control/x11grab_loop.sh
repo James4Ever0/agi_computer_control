38b821bc5ac9b4b1b00c48f545bdfc64006401fd
GitJournal <app@gitjournal.io>
2023-05-19 00:10:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab_loop.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab_loop.sh
@@ -1,2 +1,3 @@
+ps aux | grep -i xvfb | grep 11 | grep -v grep | awk '{print $2}' | xargs -iabc kill -s TERM abc
 xvfb-run --server-num=11 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority1 bash x11grab.sh
 # xvfb-run --server-num=11 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority1 bash x11grab.sh | python3 stop_on_ffmpeg_x11_error.py
