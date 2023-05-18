dfefd87997aa1aa2df1545eb1204f1e7b1341437
GitJournal <app@gitjournal.io>
2023-05-19 00:00:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab.sh
@@ -1,3 +1,6 @@
-env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -i :10 -f image2pipe pipe:1 | ffplay -f x11grab -i - -autoexit
+while true; do
+    env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -i :10 -f image2pipe pipe:1 | ffplay -f x11grab -i - -autoexit
+    sleep 1
+done;
 # env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -i :10 -f image2pipe pipe:1
 # env XAUTHORITY=/tmp/.Xauthority ffplay -f x11grab -i :10 2>&1
