cf5f3fe95724b43adc646566544ff78a65c60232
GitJournal <app@gitjournal.io>
2023-05-19 00:06:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/x11grab.sh
@@ -1,6 +1,8 @@
 while true; do
-    env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -i :10 -f image2pipe pipe:1 | ffplay -f image2pipe -i - -autoexit
+    env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -r 10 -i :10 -f image2pipe pipe:1 | ffplay -f image2pipe -i - -autoexit
     sleep 1
 done;
+# still with some lag! need improvement.
+
 # env XAUTHORITY=/tmp/.Xauthority ffmpeg -f x11grab -i :10 -f image2pipe pipe:1
 # env XAUTHORITY=/tmp/.Xauthority ffplay -f x11grab -i :10 2>&1
