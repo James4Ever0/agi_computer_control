78fc403998c1c37ef2cb58a6bcd6e918744973fd
GitJournal <app@gitjournal.io>
2023-05-18 22:44:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
@@ -3,21 +3,21 @@ import subprocess
 # run this using xvfb.
 import os
 
-script_1 = 'xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority vboxsdl --startvm "Ubuntu 16.04"'
+# script_1 = 'xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority vboxsdl --startvm "Ubuntu 16.04"'
 
 script_2 = 'env XAUTHORITY=/tmp/.Xauthorit ffplay -f x11grab -i :10'
 
 script_3 = 'vboxmanage controlvm "Ubuntu 16.04" poweroff'
 
-import time
-import progressbar
+# import time
+# import progressbar
 
-while True:
-    p1 = subprocess.run(script_1)
-    p2 = subprocess.run(script_2)
+# while True:
+#     p1 = subprocess.run(script_1)
+#     p2 = subprocess.run(script_2)
 
-    for _ in progressbar.progressbar(range(100)):
-        time.sleep(1)
+#     for _ in progressbar.progressbar(range(100)):
+#         time.sleep(1)
     
-    os.system(script_3)
+#     os.system(script_3)
     
