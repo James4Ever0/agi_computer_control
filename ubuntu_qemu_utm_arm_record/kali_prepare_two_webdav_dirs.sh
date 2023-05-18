b2ba949d069e952ae32d0c1b5466a63c8ec094c4
GitJournal <app@gitjournal.io>
2023-05-19 00:52:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
+++ ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
@@ -4,5 +4,6 @@ tmux kill-session -t kali_prepare_two_webdav_dirs
 # tmuxp load kali_prepare_two_webdav_dirs.yaml
 
 # use absolute path.
+nohup xvfb-run --server-num=12 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority2 gnome-terminal -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml" &
 
-gnome-terminal -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml"
+# gnome-terminal -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml"
--- ubuntu_qemu_utm_arm_record/kali_vbox_control/start_vm.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/start_vm.sh
@@ -1,5 +1,5 @@
 # vboxmanage startvm "12c0e77b-5f4a-4d30-b19b-1b105d2042cf" # Ubuntu 16.04
-
+nohup xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority vboxsdl --startvm "Ubuntu 16.04" &
 # start in headless mode
 # you may not live stream this one! but vrde is available! and you can use ffmpeg for bridging!
 # vboxmanage startvm "Ubuntu 16.04" --type headless
