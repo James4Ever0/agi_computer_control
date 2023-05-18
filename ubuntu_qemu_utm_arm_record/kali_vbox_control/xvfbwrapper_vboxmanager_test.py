c53e9c05c5c47e9c50fce65ea45c19c208aae696
GitJournal <app@gitjournal.io>
2023-05-19 05:30:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfbwrapper_vboxmanager_test.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfbwrapper_vboxmanager_test.py
@@ -1,1 +1,17 @@
-import xvfbwrapper
+
+from xvfbwrapper import Xvfb
+
+vdisplay = Xvfb(width=1280, height=740)
+vdisplay.start()
+
+import os
+
+try:
+    # launch stuff inside virtual display here.
+    os.system('bash start_vm.sh')
+    import time
+    import progressbar
+    for _ in progressbar.progressbar(range(1000)):
+        time.sleep(1)
+finally:
+    vdisplay.stop()
