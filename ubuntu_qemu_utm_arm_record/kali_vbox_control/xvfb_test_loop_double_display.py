ec07e59f8505dc887b2c36d167cb0ba273313cce
GitJournal <app@gitjournal.io>
2023-05-18 22:38:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
@@ -6,8 +6,16 @@ script_1 = 'xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x
 
 script_2 = 'env XAUTHORITY=/tmp/.Xauthorit ffplay -f x11grab -i :10'
 
+script_3 = 'vboxmanage '
+
 import time
+import progressbar
 
 while True:
-    p1 = subprocess.Popen(script_1)
-    p2 = subproc
+    p1 = subprocess.run(script_1)
+    p2 = subprocess.run(script_2)
+
+    for _ in progressbar.progressbar(range(100)):
+        time.sleep(1)
+    
+    os.system(script_3)
