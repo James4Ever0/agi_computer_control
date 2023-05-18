4926c8f297c4859956e8fa67e98b4e62060d587a
GitJournal <app@gitjournal.io>
2023-05-18 22:36:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfb_test_loop_double_display.py
@@ -2,4 +2,12 @@ import subprocess
 
 # run this using xvfb.
 
-"xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority vboxsdl --startvm "Ubuntu 16.04""
+script_1 = 'xvfb-run --server-num=10 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority vboxsdl --startvm "Ubuntu 16.04"'
+
+script_2 = 'env XAUTHORITY=/tmp/.Xauthorit ffplay -f x11grab -i :10'
+
+import time
+
+while True:
+    p1 = subprocess.Popen(script_1)
+    p2 = subproc
