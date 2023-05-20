4cba531b5714683008eda1691b28cd02c7220a21
GitJournal <app@gitjournal.io>
2023-05-21 02:04:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm.sh
@@ -1,5 +1,6 @@
 # vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --exe /usr/bin/env
-vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --exe /bin/loginctl
+# vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --exe /bin/loginctl
+vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run -- /bin/loginctl unlock-sessions
 
 # Class=user
 # Class=greeter
