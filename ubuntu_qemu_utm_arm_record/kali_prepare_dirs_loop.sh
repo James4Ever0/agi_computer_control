45eaeef153e0f65e0d1626be51ef863f06735fdc
GitJournal <app@gitjournal.io>
2023-05-21 01:08:01 +0800

    update

---
+++ ubuntu_qemu_utm_arm_record/kali_prepare_dirs_loop.sh
@@ -0,0 +1,5 @@
+cd '/media/root/Toshiba XG3/works/agi_computer_control'
+while true; do
+    cd .
+    tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml"
+done
--- ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
+++ ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.sh
@@ -4,7 +4,7 @@ tmux kill-session -t kali_prepare_two_webdav_dirs
 # tmuxp load kali_prepare_two_webdav_dirs.yaml
 
 # use absolute path.
-nohup xvfb-run --server-num=12 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority2 gnome-terminal --display=12 -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml" & 
+# nohup xvfb-run --server-num=12 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority2 gnome-terminal --display=12 -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml" & 
 # nohup xvfb-run --server-num=12 --listen-tcp --server-args="-screen 0 1280x800x24" -f /tmp/.Xauthority2 gnome-terminal --display=12 -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml" & 
 
 # gnome-terminal -- tmuxp load "/media/root/Toshiba XG3/works/agi_computer_control/ubuntu_qemu_utm_arm_record/kali_prepare_two_webdav_dirs.yaml"
