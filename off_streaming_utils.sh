b6e05dc0e507bc2208e1e3a69f349c0e7c3e345b
GitJournal <app@gitjournal.io>
2023-05-21 01:34:01 +0800

    update

--- off_streaming_utils.sh
+++ off_streaming_utils.sh
@@ -1,3 +1,3 @@
-tmuxp kill -p streaming_utils
+tmux kill-session -t streaming_utils
 ps aux | grep bash | grep -v grep | grep kali_prepare_dirs_loop.sh | awk '{print $2}' | xargs -iabc kill -s TERM abc
-tmuxp kill -p kali_prepare_two_webdav_dirs
+tmux kill-session -t kali_prepare_two_webdav_dirs
