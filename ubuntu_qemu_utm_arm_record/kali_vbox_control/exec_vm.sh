8099c2f086520871b78d16e9a4d0e39d5ec9ad1f
GitJournal <app@gitjournal.io>
2023-05-21 01:58:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm.sh
@@ -1,2 +1,5 @@
 # vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --exe /usr/bin/env
 vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --exe /bin/loginctl
+
+# Class=user
+# Class=greeter
