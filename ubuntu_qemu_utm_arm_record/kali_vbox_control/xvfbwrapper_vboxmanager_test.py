d4e43caaf1b734fbd34ce3994f668c22ae7a8975
GitJournal <app@gitjournal.io>
2023-05-20 16:16:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfbwrapper_vboxmanager_test.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/xvfbwrapper_vboxmanager_test.py
@@ -3,19 +3,21 @@ from xvfbwrapper import Xvfb
 
 # it sucks!
 
-vdisplay = Xvfb(display=3)
-vdisplay.start()
+# vdisplay = Xvfb(display=3)
+# vdisplay.start()
 
 # import os
 
 cmd = 'vboxsdl --startvm "Ubuntu 16.04"' 
 
-try:
-    # launch stuff inside virtual display here.
-    # os.system('bash start_vm.sh')
-    import time
-    import progressbar
-    for _ in progressbar.progressbar(range(1000)):
-        time.sleep(1)
-finally:
-    vdisplay.stop()
+with Xvfb(display=3) as vdisplay:
+
+# try:
+#     # launch stuff inside virtual display here.
+#     # os.system('bash start_vm.sh')
+#     import time
+#     import progressbar
+#     for _ in progressbar.progressbar(range(1000)):
+#         time.sleep(1)
+# finally:
+#     vdisplay.stop()
