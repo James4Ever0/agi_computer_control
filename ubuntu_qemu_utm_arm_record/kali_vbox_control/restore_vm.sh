dc691b55620f94478b8399d8803143b673d8327a
GitJournal <app@gitjournal.io>
2023-05-21 18:14:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/restore_vm.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/restore_vm.sh
@@ -1,7 +1,7 @@
 # take snapshot
 # vboxmanage snapshot "Ubuntu 16.04" take "Snapshot 3"
 # restore snapshot
-vboxmanage snapshot "Ubuntu 16.04" restore "Snapshot 13"
+vboxmanage snapshot "Ubuntu 16.04" restore "Snapshot 14"
 # 12 has bug.
 # vboxmanage snapshot "Ubuntu 16.04" restore "Snapshot 7"
 # vboxmanage snapshot "Ubuntu 16.04" restore "Snapshot 6"
