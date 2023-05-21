601c87fb97b688c3f88be15bc0719644945cecee
GitJournal <app@gitjournal.io>
2023-05-22 06:10:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/kali_vbox_main_loop.py
@@ -1,3 +1,4 @@
+from exec_vm_if_locked import exec_vm_if_locked
 import time
 import os
 import progressbar
@@ -8,8 +9,8 @@ stop_vm = "stop_vm.sh"
 start_vm = "start_vm.sh"
 kali_prepare_two_webdav_dirs = "../kali_prepare_two_webdav_dirs.sh"
 ABORT_THRESHOLD = 15
+QUIT_THRESHOLD = 30
 
-from exec_vm_if_locked import exec_vm_if_locked
 
 def run_bash_script(script_path):
     print("Excuting bash script: %s" % script_path)
@@ -21,6 +22,7 @@ def run_bash_script(script_path):
 scripts = [stop_vm, kali_prepare_two_webdav_dirs, restore_vm, start_vm]
 
 while True:
+    os.system("cd .")
     os.system("rm nohup.out")
     codes = []
     for script in scripts:
@@ -49,9 +51,21 @@ while True:
     print("WAITING {} SECONDS...".format(seconds))
     abort = 0
     quit = 0
+    old_rec_paths = os.listdir(
+        "/media/root/Toshiba XG3/works/agi_computer_control/recordings")
     for _ in progressbar.progressbar(range(seconds)):
         # you just don't wait...
         paths = os.listdir("/tmp/.X11-unix")
+        rec_paths = os.listdir(
+            "/media/root/Toshiba XG3/works/agi_computer_control/recordings")
+        if rec_paths == old_rec_paths:
+            quit += 1
+            if quit > QUIT_THRESHOLD:  # thirty secs.
+                print("ABORTING! NOT RECORDING.")
+                break
+        else:
+            old_rec_paths = rec_paths.copy()
+
         target_path = "X10"  # where virtualbox runs.
         vm_running = target_path in paths
         if not vm_running:
@@ -59,7 +73,7 @@ while True:
         else:
             reboot = exec_vm_if_locked()
             if reboot:
-                abort +=1
+                abort += 1
             else:
                 abort = 0
         if abort > ABORT_THRESHOLD:
--- ubuntu_qemu_utm_arm_record/kali_vbox_control/stop_vm.sh
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/stop_vm.sh
@@ -1,5 +1,13 @@
 timeout 10 vboxmanage controlvm "12c0e77b-5f4a-4d30-b19b-1b105d2042cf" poweroff # Ubuntu 16.04
 
+# 掉盘了 一般是
+# 就没有更好的固态硬盘盒么
+
+if [ "$?" -ne 0 ]; then
+echo "ERROR: Failed to stop vm."
+ps aux | grep -i /usr/lib/virtualbox | grep -v grep | awk '{print $2}' | xargs -iabc kill -s TERM abc
+fi
+
 # start in headless mode
 # you may not live stream this one! but vrde is available! and you can use ffmpeg for bridging!
 # vboxmanage startvm "Ubuntu 16.04" --type headless
