54616ab5665df2bc183d2082568e590a9f18fb09
GitJournal <app@gitjournal.io>
2023-05-21 15:14:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/mount_kali_webdav_dirs.sh
+++ ubuntu_qemu_utm_arm_record/mount_kali_webdav_dirs.sh
@@ -1,5 +1,12 @@
 echo -e "110110\n" | sudo -S echo "enable sudo"
 sudo apt install -y davfs2
+
+sudo umount /mnt/dav1
+sudo umount /mnt/dav2
+
+sudo rm -rf /mnt/dav1
+sudo rm -rf /mnt/dav2
+
 sudo mkdir /mnt/dav1
 sudo mkdir /mnt/dav2
 echo -e "root\nroot\n" | sudo mount -t davfs -o noexec http://10.0.1.6:8110 /mnt/dav1
