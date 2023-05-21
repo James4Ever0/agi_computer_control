4e2bd3c403c33dcd8a6eeb9c6e8ac231c01f7be2
GitJournal <app@gitjournal.io>
2023-05-21 21:46:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -1,9 +1,12 @@
 import subprocess
 
 # cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
-cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
+cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
+# cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
 
 timeout = 1 # seconds.
 output = subprocess.check_output(cmd, timeout=timeout)
 # if with error return code, it will raise exception.
-print('OUTPUT',output)
+# machine not started, service not running.
+print('=============OUTPUT=============')
+print(output.decode())
