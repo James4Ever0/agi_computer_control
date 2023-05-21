cb14bf947c577fa4cb87d73ee81a59654eb7abda
GitJournal <app@gitjournal.io>
2023-05-21 21:42:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -1,5 +1,9 @@
 import subprocess
 
+# cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
+cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
 
 timeout = 0.5 # seconds.
-subprocess.check_output(timeout=timeout)
+output = subprocess.check_output(cmd, timeout=timeout)
+
+print(output)
