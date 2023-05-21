2c7391437249a223c1fde8230ef5d252985a5ed0
GitJournal <app@gitjournal.io>
2023-05-21 14:30:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -5,93 +5,98 @@ import subprocess
 # cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
 # cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
 
+def exec_vm_if_locked():
 
-def getcmd(
-    args: list[str] = [],
-    machine="Ubuntu 16.04",
-    username="hua",
-    password="110110",
-    timeout="100",
-    bin="/bin/loginctl",
-):
-    cmd = [
-        "vboxmanage",
-        "guestcontrol",
-        machine,
-        "--username",
-        username,
-        "--password",
-        password,
-        "run",
-        "--timeout",
-        timeout,
-        "--",
-        bin,
+    def getcmd(
+        args: list[str] = [],
+        machine="Ubuntu 16.04",
+        username="hua",
+        password="110110",
+        timeout="100",
+        bin="/bin/loginctl",
+    ):
+        cmd = [
+            "vboxmanage",
+            "guestcontrol",
+            machine,
+            "--username",
+            username,
+            "--password",
+            password,
+            "run",
+            "--timeout",
+            timeout,
+            "--",
+            bin,
+        ]
+        return cmd + args
+
+
+    cmds = [
+        getcmd(e)
+        for e in [
+            [], # main info.
+            [
+                "show-seat",
+                "seat0",  # get ActiveSession
+            ],
+        ]
     ]
-    return cmd + args
 
 
-cmds = [
-    getcmd(e)
-    for e in [
-        [], # main info.
-        [
-            "show-seat",
-            "seat0",  # get ActiveSession
-        ],
-    ]
-]
+    cmd_unlock = lambda session: getcmd(['unlock-session', session])
 
 
-cmd_unlock = lambda session: getcmd(['unlock-session', session])
+    def sess_parse(data):
+        lines = data.split("\n")
+        mlist = []
+        for l in lines:
+            if "USER" in l:
+                continue
+            else:
+                l = l.replace("=", " ")
+                list_l = re.findall(r"\w+", l)
+                # print(list_l)
+                if list_l == []:
+                    break
+                mlist.append(list_l)
+        return mlist
 
 
-def sess_parse(data):
-    lines = data.split("\n")
-    mlist = []
-    for l in lines:
-        if "USER" in l:
-            continue
-        else:
-            l = l.replace("=", " ")
-            list_l = re.findall(r"\w+", l)
-            # print(list_l)
-            if list_l == []:
-                break
-            mlist.append(list_l)
-    return mlist
+    # json is for journal formatting.
 
 
-# json is for journal formatting.
+    timeout = 1  # seconds.
+    keys = ['session','seat']
+    datamap = {}
 
+    for index, cmd in enumerate(cmds):
+        key = keys[index]
+        output = subprocess.check_output(cmd, timeout=timeout)
+        # if with error return code, it will raise exception.
+        # machine not started, service not running.
+        print("=============OUTPUT=============")
+        print(output.decode())
+        print()
+        dec_output = output.decode()
 
-timeout = 1  # seconds.
-keys = ['session','seat']
-datamap = {}
+        mlist_output = sess_parse(dec_output)
+        rich.print(mlist_output)
+        mdict = {e[0]:e[1:] for e in mlist_output}
 
-for index, cmd in enumerate(cmds):
-    key = keys[index]
-    output = subprocess.check_output(cmd, timeout=timeout)
-    # if with error return code, it will raise exception.
-    # machine not started, service not running.
-    print("=============OUTPUT=============")
-    print(output.decode())
+        datamap[key] = mdict
+
     print()
-    dec_output = output.decode()
+    rich.print(datamap)
 
-    mlist_output = sess_parse(dec_output)
-    rich.print(mlist_output)
-    mdict = {e[0]:e[1:] for e in mlist_output}
+    active_session = datamap['seat']['ActiveSession']
 
-    datamap[key] = mdict
+    user_active_session = datamap['session'][active_session][1]
 
-print()
-rich.print(datamap)
-
-active_session = datamap['seat']['ActiveSession']
-
-user_active_session = datamap['session'][active_session][1]
-
-if user_active_session != 'hua':
-    reboot=True
-else:
+    reboot=False
+    if user_active_session != 'hua':
+        reboot=True
+    else:
+        cmd = cmd_unlock(active_session)
+        subprocess.call(cmd,timeout=timeout)
+    return reboot
