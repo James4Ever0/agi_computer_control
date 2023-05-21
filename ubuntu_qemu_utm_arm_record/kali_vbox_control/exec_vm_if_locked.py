1eebbf02f3411df3d970efa415ee3c1e7800088f
GitJournal <app@gitjournal.io>
2023-05-21 21:48:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -1,8 +1,8 @@
 import subprocess
 
 # cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
-cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
-# cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
+# cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
+cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl' ,, '--output=json',''list-sessions'']
 
 timeout = 1 # seconds.
 output = subprocess.check_output(cmd, timeout=timeout)
