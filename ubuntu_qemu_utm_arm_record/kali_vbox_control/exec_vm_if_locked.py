2418c726ccda7e3215eb3ff766b0423f4b3a898c
GitJournal <app@gitjournal.io>
2023-05-21 21:50:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -2,7 +2,14 @@ import subprocess
 
 # cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
 # cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
-cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl' ,, '--output=json',''list-sessions'']
+cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
+
+def sess_parse(data):
+    lines = data.split('\n')
+    for l in lines:
+        
+
+# json is for journal formatting.
 
 timeout = 1 # seconds.
 output = subprocess.check_output(cmd, timeout=timeout)
