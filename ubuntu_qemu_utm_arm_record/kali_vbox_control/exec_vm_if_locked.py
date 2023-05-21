746f64f74ae3b122b7f9ed40d6c73a4bbf87a8b5
GitJournal <app@gitjournal.io>
2023-05-21 13:56:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -4,13 +4,19 @@ import subprocess
 # cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
 cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
 
+import re
 def sess_parse(data):
     lines = data.split('\n')
+    mlist = []
     for l in lines:
         if "USER" in l:
             continue
         else:
-            list_l = l.replace(" ")
+            list_l = re.findall(r'\w+',l)
+            # print(list_l)
+            if list_l == []:break
+            mlist.append(list_l)
+    return mlist
 
 # json is for journal formatting.
 
@@ -20,3 +26,9 @@ output = subprocess.check_output(cmd, timeout=timeout)
 # machine not started, service not running.
 print('=============OUTPUT=============')
 print(output.decode())
+print()
+dec_output = output.decode()
+
+mlist_output = sess_parse(dec_output)
+import rich
+rich.print(mlist_output)
