e050a1308ab14d83cc545112ddc5763faed33425
GitJournal <app@gitjournal.io>
2023-05-21 14:00:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -4,7 +4,10 @@ import subprocess
 # cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
 cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl']
 
+cmd2 = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','','']
+
 import re
+
 def sess_parse(data):
     lines = data.split('\n')
     mlist = []
@@ -12,6 +15,7 @@ def sess_parse(data):
         if "USER" in l:
             continue
         else:
+            l = l.replace("="," ")
             list_l = re.findall(r'\w+',l)
             # print(list_l)
             if list_l == []:break
