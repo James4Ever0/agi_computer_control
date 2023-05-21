dc756962e6dbcd5d47025e12de548e10ee8e000f
GitJournal <app@gitjournal.io>
2023-05-21 14:34:01 +0800

    update

--- ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
+++ ubuntu_qemu_utm_arm_record/kali_vbox_control/exec_vm_if_locked.py
@@ -1,11 +1,14 @@
 import rich
 import re
 import subprocess
-
+import traceback
 # cmd = 'vboxmanage guestcontrol "Ubuntu 16.04" --username hua --password 110110 run --timeout 100 -- /bin/loginctl'
 # cmd = ['vboxmanage', 'guestcontrol', 'Ubuntu 16.04', '--username', 'hua', '--password', '110110', 'run', '--timeout', '100', '--', '/bin/loginctl','--help']
 
-def exec_vm_if_locked(verbose=False):
+def exec_vm_if_locked(verbose=False,
+    timeout = 1  # seconds.
+
+):
 
     def getcmd(
         args: list[str] = [],
@@ -66,39 +69,45 @@ def exec_vm_if_locked(verbose=False):
     # json is for journal formatting.
 
 
-    timeout = 1  # seconds.
     keys = ['session','seat']
     datamap = {}
-
-    for index, cmd in enumerate(cmds):
-        key = keys[index]
-        output = subprocess.check_output(cmd, timeout=timeout)
-        # if with error return code, it will raise exception.
-        # machine not started, service not running.
-        if verbose:
-            print("=============OUTPUT=============")
-            print(output.decode())
-            print()
-        dec_output = output.decode()
-
-        mlist_output = sess_parse(dec_output)
-        if verbose:
-            rich.print(mlist_output)
-        mdict = {e[0]:e[1:] for e in mlist_output}
-
-        datamap[key] = mdict
-    if verbose:
-        print()
-    rich.print(datamap)
-
-    active_session = datamap['seat']['ActiveSession']
-
-    user_active_session = datamap['session'][active_session][1]
-
     reboot=False
-    if user_active_session != 'hua':
-        reboot=True
-    else:
-        cmd = cmd_unlock(active_session)
-        subprocess.call(cmd,timeout=timeout)
+
+    try:
+        for index, cmd in enumerate(cmds):
+            key = keys[index]
+            output = subprocess.check_output(cmd, timeout=timeout)
+            # if with error return code, it will raise exception.
+            # machine not started, service not running.
+            if verbose:
+                print("=============OUTPUT=============")
+                print(output.decode())
+                print()
+            dec_output = output.decode()
+
+            mlist_output = sess_parse(dec_output)
+            if verbose:
+                rich.print(mlist_output)
+            mdict = {e[0]:e[1:] for e in mlist_output}
+
+            datamap[key] = mdict
+        if verbose:
+            print()
+        rich.print(datamap)
+
+        active_session = datamap['seat']['ActiveSession']
+
+        user_active_session = datamap['session'][active_session][1]
+
+        if user_active_session != 'hua':
+            reboot=True
+        else:
+            cmd = cmd_unlock(active_session)
+            subprocess.call(cmd,timeout=timeout)
+    except:
+        traceback.print_exc()
     return reboot
+
+if __name__ == '__main__':
+    reboot = exec_vm_if_locked(verbose=True)
+    print("NEED REBOOT?", reboot)
