3b625ffc906bec6da6bbf61c7494df7dd9ece46b
jamesbrown <randomvoidmail@foxmail.com>
2023-05-23 09:26:00 +0800

    update

--- propaganda/video_script/preprocess.py
+++ propaganda/video_script/preprocess.py
@@ -1,1 +1,14 @@
-import hanz
+
+spliters = ",.，。！~"
+
+output = 'script.md'
+with open('script.txt', 'r') as f:
+	data = f.read()
+
+for e in spliters:
+	data = data.replace(e, " ")
+
+data = data.strip()
+
+with open(output, 'w+') as f:
+	f.write("\n".join(data))
