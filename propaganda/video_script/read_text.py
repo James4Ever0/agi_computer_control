a8bb3fa01e17a9cf4e53ba9aad5b1b3f60d426a6
jamesbrown <randomvoidmail@foxmail.com>
2023-05-24 16:04:00 +0800

    update

--- propaganda/video_script/read_text.py
+++ propaganda/video_script/read_text.py
@@ -1,9 +1,14 @@
 import pyttsx3
-import os
+# import os
 
 dir_path = "audio"
-
+import yaml
 
 script_path = "script.yaml"
 with open(script_path,'r') as f:
     data = yaml.load(f, Loader=yaml.BaseLoader)
+
+for index, elem in enumerate(data):
+    test = elem['text']
+    output_path = f"{dir_path}/{index}.wav"
+    pyttsx3
