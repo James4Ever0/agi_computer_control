cca322a9186282358ca26cf2751c3aa006e07fd6
jamesbrown <randomvoidmail@foxmail.com>
2023-05-24 14:34:00 +0800

    update

--- propaganda/video_script/download_video.py
+++ propaganda/video_script/download_video.py
@@ -5,6 +5,16 @@ script_path = "script.yaml"
 dir_path = "video"
 
 with open(script_path,'r') as f:
-    data = yaml.load(f)
+    data = yaml.load(f, Loader=yaml.BaseLoader)
 
-os.system
+# os.system
+
+output_path= "output.json"
+import json
+import rich
+rich.print(data)
+for d in data:
+    vlink = d.get('video', None)
+    if vlink:
+        video_output = os.path.join()
+        os.system(f'ffmpeg -i "{vlink}" {video_output}')
