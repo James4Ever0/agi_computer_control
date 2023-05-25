e779c50ff666474a751f53aac01be3afb0140318
jamesbrown <randomvoidmail@foxmail.com>
2023-05-25 13:22:00 +0800

    update

--- ubuntu_qemu_utm_arm_record/vnc_unix_domain_socket.py
+++ ubuntu_qemu_utm_arm_record/vnc_unix_domain_socket.py
@@ -1,6 +1,6 @@
-from twisted.internet import reactor
+# from twisted.internet import reactor
 # print(dir(reactor))
 # reactor.connectTCP
 unix_addr = "/Users/jamesbrown"
-conn = reactor.connectUNIX(unix_addr)
-print(conn)
+# conn = reactor.connectUNIX(unix_addr)
+# print(conn)
