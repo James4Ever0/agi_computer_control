32e39900a2165ed761bc59d1d9913133ffeff474
jamesbrown <randomvoidmail@foxmail.com>
2023-06-07 15:34:00 +0800

    update

---
+++ recording_train_parse.py
@@ -0,0 +1,5 @@
+hid_timestamp_path = "recordings/2023-06-02T07_59_45.711256/hid_timestamps.json"
+
+video_timestamp_path = "recordings/2023-06-02T07_59_45.711256/video_timestamps.json"
+
+import json
