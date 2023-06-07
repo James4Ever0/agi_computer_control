d069f160543656d95b053aa5e072c1e690d07b86
jamesbrown <randomvoidmail@foxmail.com>
2023-06-07 15:38:00 +0800

    update

--- recording_train_parse.py
+++ recording_train_parse.py
@@ -3,3 +3,10 @@ hid_timestamp_path = "recordings/2023-06-02T07_59_45.711256/hid_timestamps.json"
 video_timestamp_path = "recordings/2023-06-02T07_59_45.711256/video_timestamps.json"
 
 import json
+
+def load_json(filename):
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+hid_timestamp = load_json(hid_timestamp_path)
+video_timestamp = load_json(video_timestamp_path)
