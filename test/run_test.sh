1b420005ab50c12d4708ed73c08f50ccdb640c78
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 13:52:00 +0800

    update

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,4 +1,4 @@
 # cd ..
-pytest --lf --lfnf=all --capture=none test_project.py
+pytest --lf --lfnf=all --capture=no test_project.py
 # pytest --lf --lfnf=all test_project.py
 # pytest --lf --lfnf=all --capture=tee-sys test_project.py
