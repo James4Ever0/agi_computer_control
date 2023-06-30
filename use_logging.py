1ab4373ca1dad0b439f35e7b03cb098a26ced17c
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 14:28:00 +0800

    update

--- use_logging.py
+++ use_logging.py
@@ -1,4 +1,8 @@
 import logging
 import sys
-logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+logging.basicConfig(level=logging.INFO, stream=sys.stdout) # suppress debug output.
+# DEBUG < INFO < WARNING < ERROR < CRITICAL
+# logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
+print("def")
 logging.debug("abc")
+print('2 abc')
