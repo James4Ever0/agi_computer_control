22530730cb215c0b8459dcf7d1af7a2d6e6b48a6
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 14:30:00 +0800

    update

--- use_logging.py
+++ use_logging.py
@@ -1,8 +1,15 @@
 import logging
 import sys
-logging.basicConfig(level=logging.INFO, stream=sys.stdout) # suppress debug output.
-# DEBUG < INFO < WARNING < ERROR < CRITICAL
+
+logging.warning("abc") # default level: warning.
+
+# logging.basicConfig(level=logging.INFO, stream=sys.stdout) # suppress debug output.
+# level priority: DEBUG < INFO < WARNING < ERROR < CRITICAL
 # logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 print("def")
 logging.debug("abc")
 print('2 abc')
+
+# cannot override?
+# logging.basicConfig(level=logging.DEBUG, stream=sys.stdout, force=True) # force overriding. you can set it somewhere.
+logging.debug("abc")
