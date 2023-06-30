f73a8b25a67b357792775a4f6cec126ec8b6e972
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 14:38:00 +0800

    update

--- test/test_project.py
+++ test/test_project.py
@@ -1,7 +1,22 @@
+
+"""Creating IES topology type system.
+
+Usage:
+    type_system_v2.py [(-p | --plot_only) | --version]
+    
+Options:
+    -l --level       Level of debug. 
+"""
+
+from docopt import docopt
+
+
+options = docopt(__doc__, version="1.0")
+import logging
+
+level = options.get('--level', )
 import sys
 sys.path.append("../")
-import logging
-import docopt
 
 logging.basicConfig(level=level, )
 from recording_train_parse import getTrainingData
