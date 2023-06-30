214cb968e047d9e8c3c3495f0e3f5709c44ef6d6
jamesbrown <randomvoidmail@foxmail.com>
2023-06-30 14:40:00 +0800

    update

--- test/test_project.py
+++ test/test_project.py
@@ -1,24 +1,11 @@
 
-"""Creating IES topology type system.
+# use "--log-level" in pytest.
 
-Usage:
-    type_system_v2.py [(-p | --plot_only) | --version]
-    
-Options:
-    -l --level       Level of debug. 
-"""
-
-from docopt import docopt
-
-
-options = docopt(__doc__, version="1.0")
-import logging
-
-level = options.get('--level', )
 import sys
 sys.path.append("../")
 
-logging.basicConfig(level=level, )
+import logging
+
 from recording_train_parse import getTrainingData
 
 import pytest
@@ -28,7 +15,7 @@ def basePath():
 
 def test_get_training_data(basePath:str):
     for trainingDataFrame in getTrainingData(basePath):
-        print(trainingDataFrame)
+        logging.debug(trainingDataFrame)
         
 # test fetching training data.
 
