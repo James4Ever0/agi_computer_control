79b1c1085ffbced74ce776b28fcdf44d1d534c2d
jamesbrown <randomvoidmail@foxmail.com>
2023-07-01 12:30:01 +0800

    update

--- test/test_project.py
+++ test/test_project.py
@@ -13,9 +13,8 @@ filename = "mytest.log"
 from logging.handlers import RotatingFileHandler
 
 myHandler = RotatingFileHandler(
-    filename, maxBytes=1024 * 1024 * 3, backupCount=3, encoding="utf-8", delay=False
+    filename, maxBytes=1024 * 1024 * 3, backupCount=3, encoding="utf-8"
 )
-
 myHandler.setLevel(logging.INFO) # will it log less things?
 FORMAT = "<%(name)s:%(levelname)s> [%(filename)s:%(lineno)s - %(funcName)s() ] %(message)s"
 myFormatter = logging.Formatter(fmt = FORMAT)
