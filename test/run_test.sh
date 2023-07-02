fb2e042fd67e3066ca71002f0e29111d6b6a8d29
jamesbrown <randomvoidmail@foxmail.com>
2023-07-02 19:26:00 +0800

    update

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,8 +1,10 @@
 # cd ..
 # pytest --lf --lfnf=all --capture=tee-sys test_project.py
 
-# pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
-env BETTER_EXCEPTIONS=1 pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
+pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
+
+# no much difference.
+# env BETTER_EXCEPTIONS=1 pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
 
 # pytest --lf --lfnf=all test_project.py
 # pytest --lf --lfnf=all --capture=tee-sys test_project.py
