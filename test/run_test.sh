aa3eaebadc4aba94e7ea8dcd4e2e552a2f90e752
jamesbrown <randomvoidmail@foxmail.com>
2023-07-02 19:24:00 +0800

    update

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,5 +1,8 @@
 # cd ..
 # pytest --lf --lfnf=all --capture=tee-sys test_project.py
-pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
+
+# pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
+env BETTER_EXCEPTIONS=1 pytest --lf --lfnf=all --capture=tee-sys --log-level=DEBUG test_project.py
+
 # pytest --lf --lfnf=all test_project.py
 # pytest --lf --lfnf=all --capture=tee-sys test_project.py
