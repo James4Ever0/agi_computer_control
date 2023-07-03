a6d8524fc01e95ae6badb673956ef248fc782b06
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 13:28:01 +0800

    update

--- array_shape_typecheck.py
+++ array_shape_typecheck.py
@@ -22,8 +22,8 @@ def add_arrays(
     return result
 
 
-myarr = add_arrays(arr, arr)  # no issue?
-# myarr = add_arrays(arr, arr2)  # only beartype shows issue.
+# myarr = add_arrays(arr, arr)  # no issue?
+myarr = add_arrays(arr, arr2)  # only beartype shows issue.
 reveal_type(myarr)
 
 # from jaxtyping import Array
@@ -32,9 +32,11 @@ from jax import Array  # type: ignore
 
 import jaxtyping  # type: ignore
 from typing_extensions import TypeAlias
+from typing import cast
 
 mTypeAlias: TypeAlias = jaxtyping.Float[Array, "dim1 dim2"]
-arr3: mTypeAlias = np.array([[1, 2, 3]])
+arr3 = cast(mTypeAlias, np.array([[1, 2, 3]]))
+# arr3: mTypeAlias = np.array([[1, 2, 3]])
 arr4: jaxtyping.Float[Array, "dim1 dim3"] = np.array([[1, 2, 3, 5]])
 
 
@@ -44,4 +46,4 @@ def add2(a: mTypeAlias, b: mTypeAlias) -> mTypeAlias:
 
 
 # arr5 = add2(arr3, arr4)
-arr5 = add2(arr3, arr3)
+arr5 = add2(arr3, arr3) # still not working.
