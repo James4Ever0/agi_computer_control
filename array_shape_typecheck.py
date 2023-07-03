ed7c15f60d0ab06bfdad218e6c1d32ba3d13431d
jamesbrown <randomvoidmail@foxmail.com>
2023-07-03 12:50:01 +0800

    update

--- array_shape_typecheck.py
+++ array_shape_typecheck.py
@@ -3,11 +3,12 @@ import numpy as np
 
 # from typing import Tuple
 # from numpy.typing import NDArray, DTypeLike
-from nptyping import NDArray, Shape, Float
+from nptyping import NDArray, Shape, Float # type: ignore
+from typing import Any
 
 M = 5
 N = 10
-arr: NDArray[Shape["5, 10"], Float] = np.zeros((M, N))
-arr2: NDArray[Shape["10, 5"], Float] = np.zeros((N, M))
-# myarr = arr + arr2  # no issue?
-reveal_type(arr)
+arr: NDArray[Shape["5, 10"], Any] = np.zeros((M, N))
+arr2: NDArray[Shape["10, 5"], Any] = np.zeros((N, M))
+myarr = arr + arr2  # no issue?
+reveal_type(myarr)
