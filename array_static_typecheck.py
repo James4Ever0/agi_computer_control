8948e6b21329ef2b04d1597c094961193d06ef97
jamesbrown <randomvoidmail@foxmail.com>
2023-07-05 14:06:00 +0800

    update

--- array_static_typecheck.py
+++ array_static_typecheck.py
@@ -1,7 +1,15 @@
-# link: https://taos.io/posts/Shape-typing-numpy-with-pyright-and-variadic-generics
+# link: https://taoa.io/posts/Shape-typing-numpy-with-pyright-and-variadic-generics
 
-from typing import TypeVar
+from typing import TypeVar, Any
 from typing_extensions import TypeVarTuple
 
 import numpy as np
 
+ScalarType = TypeVar("ScalarType", bound=np.generic, covariant=True)
+
+
+# if TYPE_CHECKING or sys.version_info >= (3, 9):
+
+_DType = np.dtype[ScalarType]
+
+NDArray = np.ndarray[Any, np.dtype[ScalarType]]
