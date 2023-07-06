4f602f195fb9954a09055beb05b136ee34c5a076
jamesbrown <randomvoidmail@foxmail.com>
2023-07-06 13:48:00 +0800

    update

--- array_static_typecheck.py
+++ array_static_typecheck.py
@@ -34,6 +34,7 @@ x.special_ops(a0)  # annotated will not be checked here.
 
 val: int
 val2: 'annotated_info' # <ast.Constant object at 0x101306290>
-T = TypeVar("T")
-T2 = TypeVar("T2")
-val3: Annotated[int, T*T2] # which can only be understood by some type checker.
+# T = TypeVar("T")
+# T2 = TypeVar("T2")
+val3: Annotated[int, T*T2] # which can only be understood by some type checker. passing this to sympy will be much better.
+val4: "T*T2"
