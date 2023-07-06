ea72171ef72bcf8e2190b49c09af4783f518468f
jamesbrown <randomvoidmail@foxmail.com>
2023-07-06 13:42:00 +0800

    update

--- array_check_ast_parsing.py
+++ array_check_ast_parsing.py
@@ -7,6 +7,7 @@ with open(code_path, "r") as f:
     content = f.read()
     tree = ast.parse(content)
     for el in tree.body:
+        rich.print(el.__dict__)
         # rich.print(dir(el))
         print(ann:=getattr(el, "annotation", None))
         if ann:
