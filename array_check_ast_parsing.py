cbab1e142b5dc484f0a9098b2bb223f5f943f570
jamesbrown <randomvoidmail@foxmail.com>
2023-07-06 13:02:00 +0800

    update

--- array_check_ast_parsing.py
+++ array_check_ast_parsing.py
@@ -1,8 +1,11 @@
 code_path = "array_static_typecheck.py"
 
 import ast
+import rich
 
-with open(code_path, 'r') as f:
+with open(code_path, "r") as f:
     content = f.read()
     tree = ast.parse(content)
-    print(tree)
+    for el in tree.body:
+        # rich.print(dir(el))
+        print(getattr(el, ""))
