a364761454dc48deb923df6010370a643b95f858
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 22:00:01 +0800

    update

--- pytropos_check.py
+++ pytropos_check.py
@@ -1,30 +1,30 @@
-# pip install git+https://github.com/helq/pytropos
-# pytropos pytropos_check.py
+# # pip install git+https://github.com/helq/pytropos
+# # pytropos pytropos_check.py
 
-import numpy as np
+# import numpy as np
 
-a = np.zeros((10, 6))
+# a = np.zeros((10, 6))
 
-m = 4 + 1
-n = 0 + 2
+# m = 4 + 1
+# n = 0 + 2
 
-if m == 5:
-    n = 1
-else:
-    m = 6
+# if m == 5:
+#     n = 1
+# else:
+#     m = 6
 
-b = np.ones((m, n))
-res = np.dot(a, b)  # fails here
+# b = np.ones((m, n))
+# res = np.dot(a, b)  # fails here
 
-print(res)
+# print(res)
 
-var = True
+# var = True
 
-if var:
-    b = np.zeros((3, 11))
-    res = np.dot(b, a)  # fails here
+# if var:
+#     b = np.zeros((3, 11))
+#     res = np.dot(b, a)  # fails here
 
-print(res)
+# print(res)
 
 import mock
 
@@ -40,3 +40,7 @@ def test_monkey(monkeypatch):
     
 from hypothesis import given
 from hypothesis.strategies import text
+
+@given(s = text(), s0=text())
+def test_myfun(s,s0):
+    print(s, s0)
