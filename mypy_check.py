33aae58024dda96899f6d0e3b9daf0c17da2be6f
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 13:40:00 +0800

    update

--- mypy_check.py
+++ mypy_check.py
@@ -14,17 +14,29 @@ Batch, Time = Annotated[axes.Batch, 3], Annotated[axes.Time, 5] # problem is, ho
 
 MyAxis = NewType("MyAxis", axes.Axis)
 
-from pycontract import contract
+# from pycontract import contract
 
-@contract
-def my_function(a, b):
-    """ Function description.
-        :type a: int,>0
-        :type b: list[N],N>0
-        :rtype: list[N]
-    """
-    ...
+# @contract
+# def my_function(a, b):
+#     """ Function description.
+#         :type a: int,>0
+#         :type b: list[N],N>0
+#         :rtype: list[N]
+#     """
+#     ...
 
 
-def sample_batch() -> ttf.Tensor2[uint8, Time, Batch]:
-    return tf.zeros((3, 5))
+# def sample_batch() -> ttf.Tensor2[uint8, Time, Batch]:
+#     return tf.zeros((3, 5))
+
+from typing_extensions import Annotated
+from annotated_types import Gt, Len, Predicate
+
+class MyClass:
+    age: Annotated[int, Gt(18)]                         # Valid: 19, 20, ...
+                                                        # Invalid: 17, 18, "19", 19.0, ...
+    factors: list[Annotated[int, Predicate(is_prime)]]  # Valid: 2, 3, 5, 7, 11, ...
+                                                        # Invalid: 4, 8, -2, 5.0, "prime", ...
+
+    my_list: Annotated[list[int], Len(0, 10)]           # Valid: [], [10, 20, 30, 40, 50]
+                                                        # Invalid: (1, 2), ["abc"], [0] * 20
