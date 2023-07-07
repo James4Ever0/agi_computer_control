448e3e071e19656b41f55bf1a68d322ddc47ae5e
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 13:42:00 +0800

    update

--- mypy_check.py
+++ mypy_check.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 from tensor_annotations import axes # by deepmind.
 import tensor_annotations.tensorflow as ttf
-from typing import NewType
+from typing import NewType, List
 from typing_extensions import Annotated
 
 uint8 = ttf.uint8
@@ -35,8 +35,8 @@ from annotated_types import Gt, Len, Predicate
 class MyClass:
     age: Annotated[int, Gt(18)]                         # Valid: 19, 20, ...
                                                         # Invalid: 17, 18, "19", 19.0, ...
-    factors: list[Annotated[int, Predicate(is_prime)]]  # Valid: 2, 3, 5, 7, 11, ...
-                                                        # Invalid: 4, 8, -2, 5.0, "prime", ...
+    # factors: List[Annotated[int, Predicate(is_prime)]]  # Valid: 2, 3, 5, 7, 11, ...
+    #                                                     # Invalid: 4, 8, -2, 5.0, "prime", ...
 
-    my_list: Annotated[list[int], Len(0, 10)]           # Valid: [], [10, 20, 30, 40, 50]
+    my_list: Annotated[List[int], Len(0, 10)]           # Valid: [], [10, 20, 30, 40, 50]
                                                         # Invalid: (1, 2), ["abc"], [0] * 20
