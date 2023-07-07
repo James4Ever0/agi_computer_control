bae80dfa1c353a6cd8b3ccd986350c2efdeec71f
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 09:38:00 +0800

    update

--- pyanalyze_check.py
+++ pyanalyze_check.py
@@ -1,14 +1,17 @@
-from pyanalyze.extensions import *
-# from pyanalyze.extensions import CustomCheck, Value, CanAssignContext, CanAssign
-import pyanalyze
+from typing import TYPE_CHECKING
 from typing_extensions import Annotated
 
-class LiteralOnly(CustomCheck):
-    def can_assign(self, value: Value, ctx: CanAssignContext) -> CanAssign:
-        for subval in pyanalyze.value.flatten_values(value):
-            if not isinstance(subval, pyanalyze.value.KnownValue):
-                return pyanalyze.value.CanAssignError("Value must be a literal")
-        return {}
+if TYPE_CHECKING:
+    from pyanalyze.extensions import *
+    # from pyanalyze.extensions import CustomCheck, Value, CanAssignContext, CanAssign
+    import pyanalyze
+
+    class LiteralOnly(CustomCheck):
+        def can_assign(self, value: Value, ctx: CanAssignContext) -> CanAssign:
+            for subval in pyanalyze.value.flatten_values(value):
+                if not isinstance(subval, pyanalyze.value.KnownValue):
+                    return pyanalyze.value.CanAssignError("Value must be a literal")
+            return {}
 
 def func(arg: Annotated[str, LiteralOnly()]) -> None:
     ...
