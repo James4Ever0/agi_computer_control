1fdd5f3826f3faa882c1f9dcb6e0f40cf915d8cb
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 09:40:00 +0800

    update

--- pyanalyze_check.py
+++ pyanalyze_check.py
@@ -1,25 +1,30 @@
-from typing import TYPE_CHECKING
+# from typing import TYPE_CHECKING
+import typing
+typing.TYPE_CHECKING = True
+from pyanalyze.extensions import *
+
+# from pyanalyze.extensions import CustomCheck, Value, CanAssignContext, CanAssign
+import pyanalyze
 from typing_extensions import Annotated
 
-if TYPE_CHECKING:
-    from pyanalyze.extensions import *
-    # from pyanalyze.extensions import CustomCheck, Value, CanAssignContext, CanAssign
-    import pyanalyze
 
-    class LiteralOnly(CustomCheck):
-        def can_assign(self, value: Value, ctx: CanAssignContext) -> CanAssign:
-            for subval in pyanalyze.value.flatten_values(value):
-                if not isinstance(subval, pyanalyze.value.KnownValue):
-                    return pyanalyze.value.CanAssignError("Value must be a literal")
-            return {}
+class LiteralOnly(CustomCheck):
+    def can_assign(self, value: Value, ctx: CanAssignContext) -> CanAssign:
+        for subval in pyanalyze.value.flatten_values(value):
+            if not isinstance(subval, pyanalyze.value.KnownValue):
+                return pyanalyze.value.CanAssignError("Value must be a literal")
+        return {}
+
 
 def func(arg: Annotated[str, LiteralOnly()]) -> None:
     ...
+
 
 def some_call():
     # it is actually running this. damn it!
     print("CALLING FUNCTION")
     return "abc"
+
 
 func("x")  # ok
 func(str(some_call()))  # error
