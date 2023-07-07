15be6b8e4b543f75a2feb0a4cf26cb169b1e6dbb
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 13:02:00 +0800

    update

--- mypy_check.py
+++ mypy_check.py
@@ -1,18 +1,19 @@
 import mypy.api as api
 
-result = api.run(...) # commandline args.
+result = api.run(...)  # commandline args.
 
 import tensorflow as tf
 
-from tensor_annotations import axes
+from tensor_annotations import axes # by deepmind.
 import tensor_annotations.tensorflow as ttf
 from typing import NewType
 from typing_extensions import Annotated
 
 uint8 = ttf.uint8
-Batch, Time = Annotated[axes.Batch,3], Annotated[axes.Time, 5]
+Batch, Time = Annotated[axes.Batch, 3], Annotated[axes.Time, 5] # problem is, how to share this along with function calls?
 
 MyAxis = NewType("MyAxis", axes.Axis)
 
+
 def sample_batch() -> ttf.Tensor2[uint8, Time, Batch]:
-  return tf.zeros((3, 5))
+    return tf.zeros((3, 5))
