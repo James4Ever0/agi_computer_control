e8fb067a6a82c831df06bc277822fa5739dba3be
jamesbrown <randomvoidmail@foxmail.com>
2023-07-07 15:56:01 +0800

    update

---
+++ pytropos_check.py
@@ -0,0 +1,26 @@
+# pip install git+https://github.com/helq/pytropos
+
+import numpy as np
+
+a = np.zeros( (10,6) )
+
+m = 4 + 1
+n = 0 + 2
+
+if m == 5:
+    n = 1
+else:
+    m = 6
+
+b = np.ones( (m,n) )
+res = np.dot(a, b)  # fails here
+
+print(res)
+
+var = True
+
+if var:
+    b = np.zeros( (3, 11) )
+    res = np.dot(b, a)  # fails here
+
+print(res)
