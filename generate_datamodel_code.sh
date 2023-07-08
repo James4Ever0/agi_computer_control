a2f8ffc67becc277a1b4dec9608f6443bc36a841
GitJournal <app@gitjournal.io>
2023-07-08 13:42:01 +0800

    update

--- generate_datamodel_code.sh
+++ generate_datamodel_code.sh
@@ -1,3 +1,3 @@
-datamodel-codegen --input mydatamodel.yaml --output mydatamodel.py
+datamodel-codegen --input mydatamodel.yaml --output mydatamodel.py --class-name MyModel
 
 # cannot process multiple instances of datamodel.
