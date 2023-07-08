fb4072387c7e77931a1e5fa2399bf9cd3a5f907a
GitJournal <app@gitjournal.io>
2023-07-08 12:46:01 +0800

    update

--- pytest_disable_assertion_inspection_use_better_exceptions.py
+++ pytest_disable_assertion_inspection_use_better_exceptions.py
@@ -2,16 +2,22 @@
 # env BETTER_EXCEPTIONS=1 python3 -m pytest --full-capture --assert=plain pytest_disable_assertion_inspection_use_better_exceptions.py
 # env BETTER_EXCEPTIONS=1 python3 -m pytest pytest_disable_assertion_inspection_use_better_exceptions.py
 
-# import better_exceptions
+import better_exceptions
 # # import unittest
+from pytest import ExceptionInfo
+def patch(cls, exc_info, exprinfo): ...
 
+
+ExceptionInfo.from_exc_info = patch
 # better_exceptions.hook()
+
 
 def test_mytest():
     a = 1
     b = {}
     # print(b[1])
     assert b[2] == a
+
 
 if __name__ == "__main__":
     test_mytest()
