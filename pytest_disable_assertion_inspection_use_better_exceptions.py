fc9321b07029b99ee2e31bfc2cc5d59ce4352373
GitJournal <app@gitjournal.io>
2023-07-08 12:38:01 +0800

    update

--- pytest_disable_assertion_inspection_use_better_exceptions.py
+++ pytest_disable_assertion_inspection_use_better_exceptions.py
@@ -1,5 +1,5 @@
 # commandline:
-# env BETTER_EXCEPTIONS=1 python3 -m pytest --capture=no --assert=plain pytest_disable_assertion_inspection_use_better_exceptions.py
+# env BETTER_EXCEPTIONS=1 python3 -m pytest --full-capture --assert=plain pytest_disable_assertion_inspection_use_better_exceptions.py
 # env BETTER_EXCEPTIONS=1 python3 -m pytest pytest_disable_assertion_inspection_use_better_exceptions.py
 
 # import better_exceptions
