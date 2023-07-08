4557c3ca04366e7b0dc5aa991e7fa317fc3dff4d
GitJournal <app@gitjournal.io>
2023-07-08 12:50:01 +0800

    update

--- pytest_disable_assertion_inspection_use_better_exceptions.py
+++ pytest_disable_assertion_inspection_use_better_exceptions.py
@@ -5,9 +5,12 @@
 import better_exceptions
 # # import unittest
 from pytest import ExceptionInfo
-def patch(cls, exc_info, exprinfo):
+
+def patch(exc_info, exprinfo):
+    cls = ExceptionInfo
     textlist = better_exceptions.format_exception(*exc_info)
     text = "".join(textlist)
+    print(text) # great.
     return cls(exc_info, text, _ispytest=True)
 
 ExceptionInfo.from_exc_info = patch
