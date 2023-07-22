e7405f409da65c0933feb3a760e2741c1bc2c21d
jamesbrown <randomvoidmail@foxmail.com>
2023-07-22 18:14:01 +0800

    update

--- conscious_struct.py
+++ conscious_struct.py
@@ -1047,6 +1047,9 @@ def resizeImage(im, desired_size):
 
 
 class SequentialEvalQueue:
+    # two modes
+    # mode 1: one is without 
+    # init data -> 
     ...
 ################
 # READING DATA #
