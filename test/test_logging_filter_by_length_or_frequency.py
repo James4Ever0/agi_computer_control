85b65711c02767a5669b57ca60d21b8c07eece24
jamesbrown <randomvoidmail@foxmail.com>
2023-07-24 23:20:00 +0800

    update

--- test/test_logging_filter_by_length_or_frequency.py
+++ test/test_logging_filter_by_length_or_frequency.py
@@ -1,5 +1,14 @@
 import logging
 import schedule
 
-ft = logging.Filter("myfilter") # default filter is just a string checker
-ft.filter = 
+# ft = logging.Filter("myfilter") # default filter is just a string checker
+
+class MyFilter:
+    @staticmethod
+    def filter(record:logging.LogRecord):
+        accepted=False
+        msg = record.message
+        print("MSG IN FILTER?", msg)
+        return accepted
+
+allow_logging = True
