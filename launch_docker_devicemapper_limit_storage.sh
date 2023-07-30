a3393e33e9e2d5dcecfd771f69b752b41a5644f9
GitJournal <app@gitjournal.io>
2023-07-30 20:00:01 +0800

    update

--- launch_docker_devicemapper_limit_storage.sh
+++ launch_docker_devicemapper_limit_storage.sh
@@ -1,9 +1,9 @@
 # --storage-opt is supported only for overlay over xfs with 'pquota' mount option.
-# docker run  --storage-opt size=10M --rm -it alpine
+# change "data-root" to somewhere else in /etc/docker/daemon.json.
+docker run  --storage-opt size=10M --rm -it alpine
 
 # when using devmapper, make sure size is greater than 10G (default)
 # https://docs.docker.com/storage/storagedriver/device-mapper-driver/#configure-direct-lvm-mode-for-production
-docker run --storage-opt size=11G --rm -it alpine
+# docker run --storage-opt size=11G --rm -it alpine
 
 # zfs, vfs (not a unionfs, but for testing) supports disk quota.
-# change "data-root" to somewhere else in /etc/docker/daemon.json.
