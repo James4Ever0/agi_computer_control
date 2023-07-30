b8f591456b29a74a92701819976e298b5ac277e8
GitJournal <app@gitjournal.io>
2023-07-30 20:12:01 +0800

    update

--- launch_docker_devicemapper_limit_storage.sh
+++ launch_docker_devicemapper_limit_storage.sh
@@ -1,5 +1,6 @@
 # --storage-opt is supported only for overlay over xfs with 'pquota' mount option.
 # change "data-root" to somewhere else in /etc/docker/daemon.json.
+# edit /etc/fstab and add our xfs block on new line (find uuid using blkid)
 docker run  --storage-opt size=10M --rm -it alpine
 
 # when using devmapper, make sure size is greater than 10G (default)
