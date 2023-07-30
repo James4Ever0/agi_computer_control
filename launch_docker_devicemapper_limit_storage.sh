30a7d0c01897982dc0c4b02fa5ad60f1017bfa7e
GitJournal <app@gitjournal.io>
2023-07-30 19:36:01 +0800

    update

--- launch_docker_devicemapper_limit_storage.sh
+++ launch_docker_devicemapper_limit_storage.sh
@@ -1,1 +1,6 @@
-docker run  --storage-opt size=10M --rm -it alpine
+# --storage-opt is supported only for overlay over xfs with 'pquota' mount option.
+# docker run  --storage-opt size=10M --rm -it alpine
+
+# when using devmapper, make sure size is greater than 10G (default)
+# https://docs.docker.com/storage/storagedriver/device-mapper-driver/#configure-direct-lvm-mode-for-production
+docker run --storage-opt size=11G --rm -it alpine
