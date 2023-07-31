c40b17dc0f76fb57e420d8f92647c906d67e5244
GitJournal <app@gitjournal.io>
2023-07-31 23:32:01 +0800

    update

--- install_nvidia_container_toolkit.sh
+++ install_nvidia_container_toolkit.sh
@@ -1,2 +1,8 @@
 # figure out what version of debian is for kali 2022.2
 # `uname -a` to get debian kernel version (5.16) -> bullseye (debian 11)
+# $  distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+$  distribution="debian11" \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
