10afbef87f808f4c7c85d7cf0e8e461d9c5191ab
GitJournal <app@gitjournal.io>
2023-08-01 23:50:01 +0800

    update

--- hardware_capture_hid_power_control/test_power_control.py
+++ hardware_capture_hid_power_control/test_power_control.py
@@ -1,3 +1,8 @@
 import serial
 
-ser = serial.Serial("/dev/serial/by-id")
+ser = serial.Serial("/dev/serial/by-id/usb-1a86_5523-if00-port0")
+# print(dir(ser))
+import rich
+# rich.print(ser.__dict__)]
+
+ser.
