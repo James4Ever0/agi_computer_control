0ebef3904e50ef6eab71e7d04d4a05405275c667
GitJournal <app@gitjournal.io>
2023-08-02 23:12:01 +0800

    update

--- hardware_capture_hid_power_control/test_video_capture.py
+++ hardware_capture_hid_power_control/test_video_capture.py
@@ -8,9 +8,15 @@
 # video2&3 are capture card.
 import cv2
 
-cap = cv2.VideoCapture(2)
+camera_id = 2
+cap = cv2.VideoCapture(camera_id)
 
 
+# showing values of the properties
+print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+print("CV_CAP_PROP_FRAME_FPs : '{}'".format(cap.get(cv2.CAP_PROP_FPS)))
+cv2.CAP_
 ret, frame = cap.read()
 
 # Display the resulting frame
