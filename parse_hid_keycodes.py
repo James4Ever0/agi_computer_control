05803578251d25364676cac9e1b85972de269f44
you <you@example.com>
2023-08-02 16:40:02 +0800

    update

--- parse_hid_keycodes.py
+++ parse_hid_keycodes.py
@@ -1,13 +1,20 @@
-input_path = "hardware_capture_hid_power_control/resources/decompressed/Kcom3资料/keys.txt"
+input_path = (
+    "hardware_capture_hid_power_control/resources/decompressed/Kcom3资料/keys.txt"
+)
 
 from typing import Annotated
 import beartype
 from beartype.vale import Is
+from beartype.door import is_bearable
 
 null_string = Annotated[str, Is[lambda s: len(s) == 0]]
-table_entry = Annotated[str, Is[lambda s: len(s) >0 and len(s)]]
+table_entry = Annotated[str, Is[lambda s: len(s) > 0 and len(s) < 30]]
 
-with open(input_path, 'r', encoding='utf-8') as f:
+table_header_count = 7
+
+with open(input_path, "r", encoding="utf-8") as f:
     for line in f.readlines():
         line = line.strip()
-        
+
+        if is_bearable(line, null_string):
+            header_index=-1
