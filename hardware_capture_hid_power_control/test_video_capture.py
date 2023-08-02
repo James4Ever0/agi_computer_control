ee4c34e41c59f92a180905b3fc9ef3d850b0281d
GitJournal <app@gitjournal.io>
2023-08-02 23:18:01 +0800

    update

--- hardware_capture_hid_power_control/test_video_capture.py
+++ hardware_capture_hid_power_control/test_video_capture.py
@@ -5,6 +5,9 @@
 # ls /sys/class/video4linux
 # v4l2-ctl --list-devices
 
+# get resolution/modes
+# v4l2-ctl --list-formats-ext
+
 # video2&3 are capture card.
 import cv2
 
@@ -16,7 +19,10 @@ cap = cv2.VideoCapture(camera_id)
 print("CV_CAP_PROP_FRAME_WIDTH: '{}'".format(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
 print("CV_CAP_PROP_FRAME_HEIGHT : '{}'".format(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 print("CV_CAP_PROP_FRAME_FPS : '{}'".format(cap.get(cv2.CAP_PROP_FPS)))
+print("CV_CAP_PROP_FRAME_FOURCC : '{}'".format(cap.get(cv2.CAP_PROP_FOURCC)))
+print("CV_CAP_PROP_FRAME_FORMAT : '{}'".format(cap.get(cv2.CAP_PROP_FORMAT)))
 # cv2.CAP_PROP_SETTINGS
+
 ret, frame = cap.read()
 
 # Display the resulting frame
