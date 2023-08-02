c26a3e9bd81c102ff4910b46dbed5a6528cbc5a7
you <you@example.com>
2023-08-02 16:35:03 +0800

    update

--- parse_hid_keycodes.py
+++ parse_hid_keycodes.py
@@ -1,5 +1,13 @@
 input_path = "hardware_capture_hid_power_control/resources/decompressed/Kcom3资料/keys.txt"
 
-with open(input_path,'r', encoding='utf-8') as f:
+from typing import Annotated
+import beartype
+from beartype.vale import Is
+
+null_string = Annotated[str, Is[lambda s: len(s) == 0]]
+table_entry = Annotated[str, Is[lambda s: len(s) >0 and len(s)]]
+
+with open(input_path, 'r', encoding='utf-8') as f:
     for line in f.readlines():
+        line = line.strip()
         
