4a78be06e3c10182f5e475a119dc16216a8666b6
you <you@example.com>
2023-08-02 16:30:02 +0800

    update

---
+++ parse_hid_keycodes.py
@@ -0,0 +1,5 @@
+input_path = "hardware_capture_hid_power_control/resources/decompressed/Kcom3资料/keys.txt"
+
+with open(input_path,'r', encoding='utf-8') as f:
+    for line in f.readlines():
+        
