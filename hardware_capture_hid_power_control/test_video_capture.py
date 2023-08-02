11f02a6f5be67d6929ff99dc695b7e0bae79607a
GitJournal <app@gitjournal.io>
2023-08-02 23:36:02 +0800

    update

--- hardware_capture_hid_power_control/test_video_capture.py
+++ hardware_capture_hid_power_control/test_video_capture.py
@@ -11,14 +11,16 @@
 # video2&3 are capture card.
 import cv2
 
-# camera_id = 2
+# can't you reset?
+camera_id = 2
 # CV_CAP_PROP_FRAME_WIDTH: '640.0'
 # CV_CAP_PROP_FRAME_HEIGHT : '480.0'
 # CV_CAP_PROP_FRAME_FPS : '30.0'
 # CV_CAP_PROP_FRAME_FOURCC : '1196444237.0'
 # CV_CAP_PROP_FRAME_FORMAT : '16.0'
 
-camera_id = 3
+# camera_id = 0
+# cam 1,3 not working.
 
 cap = cv2.VideoCapture(camera_id)
 
