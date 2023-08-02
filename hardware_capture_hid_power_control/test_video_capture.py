3be023ae9d91a9baff1d22eee7f06c2393b527cf
GitJournal <app@gitjournal.io>
2023-08-02 23:10:01 +0800

    update

--- hardware_capture_hid_power_control/test_video_capture.py
+++ hardware_capture_hid_power_control/test_video_capture.py
@@ -4,3 +4,15 @@
 # ls /dev/video*
 # ls /sys/class/video4linux
 # v4l2-ctl --list-devices
+
+# video2&3 are capture card.
+import cv2
+
+cap = cv2.VideoCapture(2)
+
+
+ret, frame = cap.read()
+
+# Display the resulting frame
+cv2.imshow('frame', frame)
+cv2.waitKey(0)
