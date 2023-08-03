75097dbd6ce23b1a05c6e36e5bd763b39ddb4f2b
you <you@example.com>
2023-08-03 11:55:03 +0800

    update

---
+++ hardware_capture_hid_power_control/common_keycodes.py
@@ -0,0 +1,18 @@
+import sys
+sys.append("../")
+
+from conscious_struct import HIDActionTypes
+import json
+
+with open("keys.json", 'r') as f:
+    content = f.read()
+    kcom_keycodes = json.loads(content)
+
+kcom_translation_table = {}
+
+for record in kcom_keycodes:
+    keyname = record[]
+    keycode = record[])
+
+def KeyLiteralToKCOMKeycode(keyLiteral: HIDActionTypes.keys):
+    
