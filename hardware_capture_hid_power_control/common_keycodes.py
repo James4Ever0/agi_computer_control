dd017139d058f6c4e87296b8c78dc332bb32cb7c
you <you@example.com>
2023-08-03 15:10:02 +0800

    update

--- hardware_capture_hid_power_control/common_keycodes.py
+++ hardware_capture_hid_power_control/common_keycodes.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.append("../")
 
 # TODO: add feedback to unsupported keys.
@@ -31,28 +32,33 @@ for record in kcom_keycodes:
     keyname = keyname.replace("Arrow", "").strip()
 
     base_trans0 = keyname.replace(" ", "_").lower()
-    base_trans = base_trans0.replace("gui", "cmd").replace("control", 'ctrl').replace("escape", 'esc').replace("keyboard", "media").replace("mute", "volume_mute").replace("volume_dn", "volume_down").replace('return', 'enter')
+    base_trans = (
+        base_trans0.replace("gui", "cmd")
+        .replace("control", "ctrl")
+        .replace("escape", "esc")
+        .replace("keyboard", "media")
+        .replace("mute", "volume_mute")
+        .replace("volume_dn", "volume_down")
+        .replace("return", "enter")
+    )
+
     def do_append(t):
         possible_translations.append(t)
         possible_translations.append(f"Key.{t}")
+
     do_append(base_trans)
     for direction in ["right_", "left_"]:
         if base_trans.startswith(direction):
-            base_trans = base_trans.replace(direction,"") + f"_{direction[0]}"
-            if direction =="left_":
+            base_trans = base_trans.replace(direction, "") + f"_{direction[0]}"
+            if direction == "left_":
                 basekey = base_trans.split("_")[0]
                 do_append(basekey)
             do_append(base_trans)
-    if (
-        not base_trans0.startswith("F")
-    ):
-        if (len(keyname) == 3
-        and keyname[1] == " "
-    ):
+    if not base_trans0.startswith("F"):
+        if len(keyname) == 3 and keyname[1] == " ":
             val = keyname[0]
             trans = f"""'{val}'""" if val != "'" else f'''"{val}"'''
             possible_translations.append(trans)
-
 
     for translation in possible_translations:
         kcom_translation_table[translation] = keycode
@@ -65,10 +71,26 @@ def KeyLiteralToKCOMKeycode(keyLiteral: HIDActionTypes.keys):
 if __name__ == "__main__":
     # coverage test.
     error_msg = []
-    missing_key_literals = []
+    missing_key_literals = [
+        "Key.media_play_pause",
+        "Key.media_previous",
+        "Key.media_next",
+    ]
+    translation_table_cleaned = {}
+    import rich
     for key_literal in HIDActionTypes.keys.__args__:
         if key_literal not in kcom_translation_table:
             if key_literal not in missing_key_literals:
                 error_msg.append(f"{key_literal} not covered by translation table.")
+        else:
+            keycode = kcom_translation_table[key_literal]
+            translation_table_cleaned.update({key_literal:keycode})
     if error_msg:
-        raise Exception("\n"+"\n".join(error_msg))
+        raise Exception("\n" + "\n".join(error_msg))
+    print("cleaned translation table:")
+    rich.print(translation_table_cleaned)
+    output_data = {"translation_table": translation_table_cleaned, "missing":missing_key_literals}
+    with open(out:="translation_keys.json", 'w+') as f:
+        content = json.dumps(output_data)
+        f.write(content)
+    print("write to:", out)
