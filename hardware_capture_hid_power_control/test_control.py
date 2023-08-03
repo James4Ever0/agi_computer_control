8aaed31a72ab99375e9867eb0b4b031b27847c30
you <you@example.com>
2023-08-03 17:30:02 +0800

    update

--- hardware_capture_hid_power_control/test_control.py
+++ hardware_capture_hid_power_control/test_control.py
@@ -344,6 +344,71 @@ elif deviceType == DeviceType.ch9329:
     
     keyboard = ch9329Comm.keyboard.DataComm()
     mouse = ch9329Comm.mouse.DataComm(screen_width=1920, screen_height=1080)
+
+    # monkey patch some method.
+    
+    @beartype
+    def send_data(self, 
+        control_codes: List[ControlCode] = [],
+        key_literals: Annotated[
+            List[HIDActionTypes.keys], Is[lambda l: len(l) <= 6 and len(l) >= 0]
+        ] = [], port=serial):
+        # 将字符转写为数据包
+        HEAD = b'\x57\xAB'  # 帧头
+        ADDR = b'\x00'  # 地址
+        CMD = b'\x02'  # 命令
+        LEN = b'\x08'  # 数据长度
+        DATA = b''  # 数据
+
+        # 控制键
+        if ctrl == '':
+            DATA += b'\x00'
+        elif isinstance(ctrl, int):
+            DATA += bytes([ctrl])
+        else:
+            DATA += self.control_button_hex_dict[ctrl]
+
+        # DATA固定码
+        DATA += b'\x00'
+
+        # 读入data
+        for i in range(0, len(data), 2):
+            DATA += self.normal_button_hex_dict[data[i:i + 2]]
+        if len(DATA) < 8:
+            DATA += b'\x00' * (8 - len(DATA))
+        else:
+            DATA = DATA[:8]
+
+        # 分离HEAD中的值，并计算和
+        HEAD_hex_list = []
+        for byte in HEAD:
+            HEAD_hex_list.append(byte)
+        HEAD_add_hex_list = sum(HEAD_hex_list)
+
+        # 分离DATA中的值，并计算和
+        DATA_hex_list = []
+        for byte in DATA:
+            DATA_hex_list.append(byte)
+        DATA_add_hex_list = sum(DATA_hex_list)
+
+        #
+        try:
+            SUM = sum([HEAD_add_hex_list, int.from_bytes(ADDR, byteorder='big'),
+                       int.from_bytes(CMD, byteorder='big'), int.from_bytes(LEN, byteorder='big'),
+                       DATA_add_hex_list]) % 256  # 校验和
+        except OverflowError:
+            print("int too big to convert")
+            return False
+        packet = HEAD + ADDR + CMD + LEN + DATA + bytes([SUM])  # 数据包
+        port.ser.write(packet)  # 将命令代码写入串口
+        return True  # 如果成功，则返回True，否则引发异常
+
+    
+    from types import MethodType
+
+    # to override instance methods.
+    keyboard.send_data = MethodType(send_data, keyboard)
+
 else:
     raise Exception("Unknown device type: {deviceType}".format(deviceType=deviceType))
 
