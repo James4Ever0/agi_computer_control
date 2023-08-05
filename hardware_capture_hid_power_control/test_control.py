b167c726faf852c3216d0bbabfc0ecc8d4431691
GitJournal <app@gitjournal.io>
2023-08-05 23:48:01 +0800

    update

--- hardware_capture_hid_power_control/test_control.py
+++ hardware_capture_hid_power_control/test_control.py
@@ -39,23 +39,6 @@ movement: TypeAlias = Annotated[int, Is[lambda i: i >= -126 and i <= 126]]
 # confusing!
 
 # TODO: unit test underway
-# TODO: more control methods (non-hardware) under way
-# vnc/rdp (rdpy (py2), rdpy3, python3-aardwolf, rdesktop (rdp)) (docker-vnc image: dorowu/ubuntu-desktop-lxde-vnc:focal ; docker rdp image: scottyhardy/docker-remote-desktop)
-# ssh (terminal interface)
-# spice (remmina, remote-viewer (RHEL))
-# xvfb (with pyautogui?) (use vglrun (GPU)) (what alternatives to xvfb are for macOS and Windows?)
-# -----------[use remote control methods as self control methods]-----------
-# self control (pyautogui, pynput, (win)tty, tmux, subprocess, ttyd with electron/xvfb based browser client)
-
-# qtpy: pyqt4/5/6 abstraction layer
-# https://github.com/spyder-ide/qtpy
-
-# docker-wine image (in case running windows app on linux): scottyhardy/docker-wine
-
-# MineRL GPU rendering: https://minerl.readthedocs.io/en/latest/notes/performance-tips.html
-
-# rdpy3: https://github.com/massimiliano-dalcero/rdpy
-# ref: https://github.com/citronneur/rdpy/issues/91
 
 @beartype
 def get_scroll_code(c_scroll: movement) -> one_byte:
---
+++ software_capture_hid_control/test_control.py
@@ -0,0 +1,19 @@
+# TODO: more control methods (non-hardware) under way
+# vnc/rdp (rdpy (py2), rdpy3, python3-aardwolf, rdesktop (rdp)) (docker-vnc image: dorowu/ubuntu-desktop-lxde-vnc:focal ; docker rdp image: scottyhardy/docker-remote-desktop)
+# ssh (terminal interface)
+# spice (remmina, remote-viewer (RHEL))
+# xvfb (with pyautogui?) (use vglrun (GPU)) (what alternatives to xvfb are for macOS and Windows?)
+# -----------[use remote control methods as self control methods]-----------
+# self control (pyautogui, pynput, (win)tty, tmux, subprocess, ttyd with electron/xvfb based browser client)
+
+# qtpy: pyqt4/5/6 abstraction layer
+# https://github.com/spyder-ide/qtpy
+
+# docker-wine image (in case running windows app on linux): scottyhardy/docker-wine
+
+# MineRL GPU rendering: https://minerl.readthedocs.io/en/latest/notes/performance-tips.html
+
+# rdpy3: https://github.com/massimiliano-dalcero/rdpy
+# ref: https://github.com/citronneur/rdpy/issues/91
+
+# shall you look over our previous project 
