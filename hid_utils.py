f03b98681a934dae6adff03536b78c47ba181158
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 14:50:01 +0800

    update

--- hid_utils.py
+++ hid_utils.py
@@ -33,7 +33,9 @@ def strip_key_literal(key_literal: HIDActionTypes.keys):
         return is_special, is_media, keychar
 
 @beartype
-def xk_keysym
+def key_literal_to_xk_keysym(key_literal: HIDActionTypes.keys):
+    is_special, is_media, stripped_key_literal = strip_key_literal(key_literal)
+    Xlib.XK.string_to_keysym(stripped_key_literal)
 
 def byte_with_length_limit(l):
     return Annotated[bytes, length_limit(l)]
