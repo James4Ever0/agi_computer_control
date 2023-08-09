8f9b888ec4c999d53b46613c9ae3f12f7e7e6cd4
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 10:12:00 +0800

    update

--- test/test_abstract_impl.py
+++ test/test_abstract_impl.py
@@ -9,4 +9,9 @@ class abs_class(ABC):
 
 class impl_class(abs_class):
     def _method_impl(self):
-        
+        return 1
+class impl_class2(abs_class):
+    def non_relevant(self):
+        ...
+print(impl_class().method())
+# print(impl_class2())
