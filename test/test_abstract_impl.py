14d157220c4bda41b705b1a3c732b8bca35430c6
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 10:28:00 +0800

    update

--- test/test_abstract_impl.py
+++ test/test_abstract_impl.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 class abs_class(ABC):
     def method(self):
+        "doc here"
         return self._method_impl()
     @abstractmethod
     def _method_impl(self):
