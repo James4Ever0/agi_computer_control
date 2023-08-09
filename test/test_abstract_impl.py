9ac7ce24a7021a823da3c7515a8111fc99e81a98
jamesbrown <randomvoidmail@foxmail.com>
2023-08-09 10:42:01 +0800

    update

--- test/test_abstract_impl.py
+++ test/test_abstract_impl.py
@@ -1,19 +1,26 @@
 from abc import ABC, abstractmethod
 
+
 class abs_class(ABC):
     def method(self):
         "doc here"
         return self._method_impl()
+
     @abstractmethod
     def _method_impl(self):
         ...
 
+
 class impl_class(abs_class):
     def _method_impl(self):
         return 1
+
+
 class impl_class2(abs_class):
     def non_relevant(self):
         ...
+
+
 print(impl_class().method())
 # print(impl_class2()) # error
 # print(abs_class()) # error
