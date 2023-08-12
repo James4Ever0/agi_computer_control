a2c4d801dd6360b93942f29c2477457d45e98df1
GitJournal <app@gitjournal.io>
2023-08-12 20:16:01 +0800

    update

--- usb_power_management/linux/devcontrol.sh
+++ usb_power_management/linux/devcontrol.sh
@@ -1,3 +1,4 @@
+# ref: https://www.kernel.org/doc/html/v4.16/driver-api/usb/power-management.html
 cd /sys/bus/usb/devices
 ls -1 */power/control | xargs -Iabc bash -c "echo on > abc"
 echo -1 > /sys/module/usbcore/parameters/autosuspend
