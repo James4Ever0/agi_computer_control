8223274b9b1376dcabffd8bf8ec7d22915edfffd
GitJournal <app@gitjournal.io>
2023-08-12 20:14:01 +0800

    update

--- usb_power_management/linux/devcontrol.sh
+++ usb_power_management/linux/devcontrol.sh
@@ -2,5 +2,5 @@ cd /sys/bus/usb/devices
 ls -1 */power/control | xargs -Iabc bash -c "echo on > abc"
 echo -1 > /sys/module/usbcore/parameters/autosuspend
 ls -1 */power/usb2_hardware_lpm | xargs -Iabc bash -c "echo n > abc"
-ls -1 */power/usb3_hardware_lpm_u1 | xargs -Iabc bash -c "echo disable > abc"
-ls -1 */power/usb3_hardware_lpm_u1 | xargs -Iabc bash -c "echo disable > abc"
+# ls -1 */power/usb3_hardware_lpm_u1 | xargs -Iabc bash -c "echo disable > abc"
+# ls -1 */power/usb3_hardware_lpm_u2 | xargs -Iabc bash -c "echo disable > abc"
