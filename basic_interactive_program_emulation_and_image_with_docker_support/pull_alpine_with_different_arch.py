a6ee6e6d0b4a9480d71e72940c16ad12f519f05d
you <you@example.com>
2023-08-30 15:10:02 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/pull_alpine_with_different_arch.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/pull_alpine_with_different_arch.py
@@ -3,11 +3,15 @@ target_image = "alpine:3.7"
 target_fname_prefix = target_image.replace(":", "_")
 import os
 
+dirpath = "docker_images"
+if os.path.exists(dirpath):
+    raise Exception(f"target directory '{dirpath}' already exists.")
+os.mkdir(dirpath)
 for arch in arch_list:
     fpath = f"{target_fname_prefix}_{arch.replace('/','')}.tar"
     cmds = [
         f'docker pull --platform "linux/{arch}" {target_image}',
-        f"docker save {target_image} -o {fpath}",
+        f"docker save {target_image} -o {dirpath}/{fpath}",
         f"docker rmi {target_image}",
     ]
     for cmd in cmds:
