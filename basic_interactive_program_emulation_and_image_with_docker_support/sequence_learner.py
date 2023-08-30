f44e8d001825d11c9dc464a9cdcfb932a2227108
you <you@example.com>
2023-08-30 17:20:02 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/sequence_learner.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/sequence_learner.py
@@ -19,17 +19,36 @@ class NaivePredictor:
     def one_hot(self, x):
         x_one_hot = np.eye(self.ksize)[x]
         return x_one_hot
-
-
+    
 from collections import deque
 
+class PredictorWrapper:
+    def __init__(self, ksize, predictor_cls):
+        self.predictor = predictor_cls(ksize)
+        self.seq= deque([], maxlen=ksize)
+    
+    def enqueue(self, seq:List[int]):
+        for tok in seq:
+            self.seq.append(tok)
+    
+    def predict(self, seqlen:int):
+        ret_seq = []
+        for _ in range(seqlen):
+            tok = self.predictor.predict(list(self.seq))
+            self.seq.append(tok)
+            ret_seq.append(tok)
+        return ret_seq
+
+
 if __name__ == "__main__":
-    ksize = 10
-    predictor = NaivePredictor(ksize=ksize)
-    seq = deque([0, 1, 2, 3, 4], maxlen=ksize)
-    total_seq = list(seq)
-    for _ in range(100):
-        tok = predictor.predict(list(seq))
-        seq.append(tok)
-        total_seq.append(tok)
-    print("total seq:", total_seq)
+    pw = PredictorWrapper(10, NaivePredictor)
+    pw.enqueue([0,1,2,3,4])
+    # ksize = 10
+    # predictor = NaivePredictor(ksize=ksize)
+    # seq = deque([0, 1, 2, 3, 4], maxlen=ksize)
+    # total_seq = list(seq)
+    # for _ in range(100):
+    #     tok = predictor.predict(list(seq))
+    #     seq.append(tok)
+    #     total_seq.append(tok)
+    # print("total seq:", total_seq)
