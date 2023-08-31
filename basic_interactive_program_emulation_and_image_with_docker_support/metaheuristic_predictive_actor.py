654066bc14cc26f6f202854d891490e10c2d7a00
you <you@example.com>
2023-08-31 11:00:02 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/metaheuristic_predictive_actor.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/metaheuristic_predictive_actor.py
@@ -1,5 +1,11 @@
 # extract the kernel from the dead ones.
 # the new kernel will be added to the random kernel.
+import numpy as np
 
 class MetaheuristicPredictiveWrapper:
-    ...
+    top_k = 100
+    def new(self,):
+        ...
+    @staticmethod
+    def score():
+        ...
