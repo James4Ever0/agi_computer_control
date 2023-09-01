3344c8990c8be8b750a73b5933cf68d5bbfda86c
you <you@example.com>
2023-09-01 09:55:02 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/metaheuristic_predictive_actor.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/metaheuristic_predictive_actor.py
@@ -2,16 +2,22 @@
 # the new kernel will be added to the random kernel.
 import numpy as np
 from sequence_learner import PredictorWrapper
+from naive_actor import ActorStats
+
 
 class MetaheuristicPredictiveWrapper:
     top_k = 100
+
     def __init__(self):
         self.wrapper = PredictorWrapper()
         self.actor = ...
-    def new(self,):
+
+    def new(
+        self,
+    ):
         del self.actor
         self.actor = ...
-    @staticmethod
-    def score():
-        stats = self.actor.stats
+
+    def score(self):
+        stats: ActorStats = self.actor.stats
         # score by what?
