2e287ad24a13964b000c7c57aa840c9e94ec6519
you <you@example.com>
2023-09-18 10:00:06 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/rerun_docker_daemon.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/rerun_docker_daemon.py
@@ -6,7 +6,7 @@ LINUX_RESTART_DOCKER_COMMAND = "sudo systemctl restart docker"
 # DOES NOT WORK ON WIN11
 # kill com.docker.backend.exe? seems to be hanging
 WINDOWS_KILL_DOCKER_COMMAND = 'taskkill /FI "IMAGENAME eq Docker*" /F'
-# start program minimized
+# start program minimized? instead use pygetwindow to hide the window once found.
 # find 'Docker Desktop.exe'
 # which docker -> ../../ -> 'Docker Desktop.exe'
 # WINDOWS_RESTART_DOCKER_COMMAND = 'powershell -Command "Restart-Service -Name *docker*"' # need elevated permissions
