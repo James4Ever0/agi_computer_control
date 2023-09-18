50ab3af7bb558490f191a4706881270c15c64b80
you <you@example.com>
2023-09-18 09:15:07 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/rerun_docker_daemon.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/rerun_docker_daemon.py
@@ -3,7 +3,9 @@ MACOS_DOCKER_APP_BINARY = "/Applications/Docker.app/Contents/MacOS/Docker"
 
 LINUX_RESTART_DOCKER_COMMAND = "sudo systemctl restart docker"
 
-WINDOWS_RESTART_DOCKER_COMMAND = 'powershell -Command "Restart-Service -Name *docker*"'
+# DOES NOT WORK ON WIN11
+# kill com.docker.backend.exe? seems to be hanging
+WINDOWS_RESTART_DOCKER_COMMAND = 'powershell -Command "Restart-Service -Name *docker*"' # need elevated permissions
 # Stop-Service & Start-Service
 # net stop com.docker.service/docker & net start com.docker.service/docker
 import platform
