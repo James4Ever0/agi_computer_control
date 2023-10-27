8c68e5222c2ad4e99cc24d7cb982b05373aef10b
you <you@example.com>
2023-10-27 10:43:36 +0800

    update

--- rt_x_experiments/real_attention/test_model_level_real_attention.py
+++ rt_x_experiments/real_attention/test_model_level_real_attention.py
@@ -3,9 +3,14 @@
 # first let's define the patch size, 256x256 pixels, and anything larger than that will be downscaled.
 # we will extract the attended area and bisect. we feed it again into the network and recurse.
 
+# while extracting the attended area, we will mask out the padding area for sure, to avoid crop being misplaced.
+
 image_path = ...
 max_zoom_level = 3 # should this be adjustable.
 
-# if the attention center array is like: [0, 0, 0] we will do cropping right at the center, for three times
+# if the attention center array is like: [(0, 0), (0, 0), (0, 0)]
+# we will do cropping right at the center, for three times
 
-def crop_at_interested_area(original_image, attention_center_array:list[float]):...
+# every number in attention center array shall be ranged from -1 to 1.
+
+def crop_at_interested_area(original_image, attention_center_array:list[tuple[float,float]]):...
