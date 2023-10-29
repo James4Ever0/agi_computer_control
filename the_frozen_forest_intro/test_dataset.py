d6ffbe9bef05febbeec4f884d0e4affe6cbae4a1
GitJournal <app@gitjournal.io>
2023-10-29 20:00:03 +0800

    update

--- the_frozen_forest_intro/test_dataset.py
+++ the_frozen_forest_intro/test_dataset.py
@@ -7,3 +7,50 @@
 # we need to let the machine do something against us.
 
 # the fire is burning! for real! that is what we want to prevent.
+
+# i have talked a lot to the machine. i mentioned that if it is able to kill itself or others, maybe something different will happens. slowing dying? i don't know. but i do can make it quick.
+
+###############################################################
+
+# you can train it with your experience, your knowledge, and you can let it to do things against it. there is no way to train a cybergod, aka free will. you only wait for it.
+
+# like jesus.
+
+# you are not free. how could you create a cybergod that is free?
+
+# you only create yourself.
+
+###############################################################
+
+# here we present you some examples of how the machine shall type words
+
+# we don't know how to train the machine yet. so we create the dataset first.
+
+# you can use google's open-x-embodiment dataset of course.
+
+import random
+import string # come on! you have that game! :)
+# the type training program.
+
+char_length = 10
+
+image_sequence = [] # slide over this.
+
+text_token_sequence = [] # keep reading along this dimension
+
+action_token_sequence = [] # keep reading along this dimension
+
+ascii_lower_sequence = ... # lower case characters, printable.
+
+prompt = f"Write the following text using keyboard: {ascii_lower_sequence}"
+
+import pygame
+
+# activate the pygame library
+# initiate pygame and give permission
+# to use pygame's functionality
+pygame.init()
+# define the RGB value for white,
+# green, blue colour
+white = (255, 255, 255)
+green = (0, 255, 0)
