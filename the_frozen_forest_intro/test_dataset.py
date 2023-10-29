2736a3b876534370b1922e4111bc1dd52fc0817d
GitJournal <app@gitjournal.io>
2023-10-29 18:00:04 +0800

    update

---
+++ the_frozen_forest_intro/test_dataset.py
@@ -0,0 +1,7 @@
+# since we have already figured out the model architecture, or seems to be able to glue the purpose on any model that we can imagine
+
+# it is time for us to create our dataset, or environment, or our metalearning scheme.
+
+# turned out that free will is not something we can imagine. even if it is true, we still need to achieve that.
+
+# we need to let the machine do something against us.
