3ff8b5b2e43c3c61f68a564ca261df7124984978
you <you@example.com>
2023-10-30 18:57:15 +0800

    update

--- the_frozen_forest_intro/watch_video_in_webpage.py
+++ the_frozen_forest_intro/watch_video_in_webpage.py
@@ -20,10 +20,11 @@ set_paths(
 # no sound!
 # 关闭静音开播
 
-from DrissionPage import ChromiumPage, ChromiumOptions
+from DrissionPage import ChromiumPage #, ChromiumOptions
 
-co = ChromiumOptions()
-co.set_mute(False)
+# co = ChromiumOptions()
+# co.set_mute(True)
+# co.set_mute(False)
 
 # userscripts to fix bilibili ads, for babysitting
 # https://greasyfork.org/zh-CN/scripts/467511-bilibili-%E5%9C%A8%E6%9C%AA%E7%99%BB%E5%BD%95%E7%9A%84%E6%83%85%E5%86%B5%E4%B8%8B%E8%87%AA%E5%8A%A8%E5%B9%B6%E6%97%A0%E9%99%90%E8%AF%95%E7%94%A8%E6%9C%80%E9%AB%98%E7%94%BB%E8%B4%A8
@@ -33,12 +34,13 @@ co.set_mute(False)
 
 # 用 d 模式创建页面对象（默认模式）
 page = ChromiumPage()
+# page = ChromiumPage(addr_driver_opts=co)
 # 跳转到登录页面
 page.get("https://www.bilibili.com/video/BV1J24y1c7kE") # and then it will continue execution
 
-page.set_mute
 # looks like it is the policy. let's turn it off.
 # will fail if there is no such button
+page.ele('点击恢复音量').click() # working.
 # page.ele('@value=点击恢复音量').click()
 
 # span
