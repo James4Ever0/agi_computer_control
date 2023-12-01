81d22e7b1764c2b8305e26de02d809953aa9d0ed
GitJournal <app@gitjournal.io>
2023-12-01 20:38:46 +0800

    update

---
+++ external_reference/supercharge_init.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/OthersideAI/self-operating-computer
+git clone https://github.com/tairov/QStarLearning.mojo
+git clone https://github.com/estill01/open_qstar
+git clone https://github.com/unconv/gpt4v-browsing
