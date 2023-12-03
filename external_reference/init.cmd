9d6ff04e65ad2f318114abd0e9b98a4575409b71
GitJournal <app@gitjournal.io>
2023-12-03 20:00:13 +0800

    update

--- external_reference/init.cmd
+++ external_reference/init.cmd
@@ -2,3 +2,4 @@ git clone https://github.com/abhiprojectz/SingularGPT
 git clone https://github.com/ddupont808/GPT-4V-Act
 git clone https://github.com/openai/Video-Pre-Training
 git clone https://github.com/Charmve/gpt-eyes
+git clone https://github.com/ruvnet/q-star
