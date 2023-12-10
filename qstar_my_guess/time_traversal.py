0d344501516b7bd33032ec40c2b8ac8c88495652
GitJournal <app@gitjournal.io>
2023-12-10 20:00:01 +0800

    update

--- qstar_my_guess/time_traversal.py
+++ qstar_my_guess/time_traversal.py
@@ -1,6 +1,7 @@
 import torch
 
 # heuristic: compute cosine similarity between target token and actual token
+# sequence heuristic: seqlen heuristic
 
 # v1: separated word model & control model
 init_sequence = world_model(noise)
@@ -15,10 +16,12 @@ init_sequence = real_world()
 prompt = special_token + target
 
 # use thought token as prompt
-prompt = random_thought_tokens
+prompt = get_thought_tokens(special_token + target) # thought tokens has to be "understood" by the model, so that we can know its intention (can we convert natural language prompt to thought tokens, aligned?)
 
 # perform gradient descent based on cosine similarity, more similar result to target, more learning.
 # only perform descent on the most similar one.
 # next time learn about the same pattern, we still select the most similar one, closing the gap.
+
+# a*: minimize distance from target to result + distance from source (init state) to result
 
 # the reward can also be related to "easyness". if the model is doing some easy stuff (predicted by the world model), then no intensive learning is performed. if what the model want is hard to realize in world, the model will learn hard to do it.
