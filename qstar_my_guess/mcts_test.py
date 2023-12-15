0bb94c60b82d82c8c7ad1e6ecade2ce59b3d5472
GitJournal <app@gitjournal.io>
2023-12-15 20:00:01 +0800

    update

--- qstar_my_guess/mcts_test.py
+++ qstar_my_guess/mcts_test.py
@@ -1,2 +1,93 @@
-import mcts_simple
 # any example to run?
+# import dill
+# import pickle
+# pickle.dump = dill.dump
+# pickle.load = dill.load
+
+# if the viewer-producer model is probablistic or state-machine like, we can use linear programming.
+# train multiple producers and viewers, selecting the most appropriate topics suitable for some part of the platform content.
+
+import imageio
+import base64
+
+# import IPython
+import gymnasium as gym
+from mcts_simple import *
+import sys
+
+sys.setrecursionlimit(int(1e9))  # workaroud to pickle error.
+
+
+class CartPole(Game):
+    """
+    The episode ends if any one of the following occurs:
+        * Termination: Pole Angle is greater than ±12°
+        * Termination: Cart Position is greater than ±2.4 (center of the cart reaches the edge of the display)
+        * Truncation: Episode length is greater than 500 (200 for v0)
+    """
+
+    def __init__(self):
+        # self.env = gym.make("CartPole-v1", render_mode="human")
+        self.env = gym.make("CartPole-v1", render_mode="rgb_array")
+        self.current_state, _ = self.env.reset()
+        self.frames = []
+        self.terminated, self.truncated = False, False
+
+    def render(self):
+        self.frames.append(self.env.render())
+        if self.has_outcome():
+            # IPython.display.display(
+            #     IPython.display.HTML(
+            #         data=f"""
+            # <video controls src = "data:video/mp4;base64,{base64.b64encode(imageio.mimsave(
+            # "<bytes>", self.frames, "MP4", fps = 20, **{"macro_block_size": None})).decode()}"></video>
+            # """
+            #     )
+            # )
+            self.frames.clear()
+
+    def get_state(self):
+        return self.current_state
+
+    def number_of_players(self):
+        return 1
+
+    def current_player(self):
+        return 0
+
+    def possible_actions(self):
+        return [i for i in range(self.env.action_space.n)]
+
+    def take_action(self, action):
+        if not self.has_outcome():
+            self.current_state, _, self.terminated, self.truncated, _ = self.env.step(
+                action
+            )
+
+    def has_outcome(self):
+        return self.terminated or self.truncated
+
+    def winner(self):
+        # Noting that backprop code is: node.w += (prev_node.player in winners) / number_of_winners
+        # It is possible to manipulate list size = self.env._max_episode_steps - self.env._elapsed_steps, since there will always be only 1 player
+        # winner() will return a reward instead, where 0 <= reward <= 1, where it will increase exponentially as elapsed steps increase
+        return [
+            self.current_player()
+            for _ in range(
+                max(1, self.env._max_episode_steps - self.env._elapsed_steps + 1)
+            )
+        ]
+
+
+game = CartPole()
+
+
+tree = OpenLoopMCTS(game, training=True)
+tree.self_play(iterations=1000)
+
+
+tree.training = False
+tree.self_play()
+
+
+tree.save("cartpole.mcts")  # cannot save.
--- qstar_my_guess/time_traversal.py
+++ qstar_my_guess/time_traversal.py
@@ -9,31 +9,67 @@ import torch
 # historical tokens: reverse order autoregressive model predictions, memory retrieval, past context
 # future tokens: future predictions that being made short or slow (skipping intermediate steps, or making it faster), or contradict with common future predictions (unusual, less probable future tokens)
 
-# TODO: hierarchy of control
+# TODO: hierarchy of control (by two special tokens: <abstract> and <deabstract>. one can set max abstraction level)
+# TODO: slowing down and speeding up
+output = speed_adjustment()
+# TODO: emitting multiple kinds of tokens at the same time, at separate channels
+# TODO: rollback gradient descent when no further improvement is found
 
 # v1: separated world model & control model
+
 init_sequence = world_model(noise)
+action_sequence = action_model(init_sequence, prompt)
+continue_sequence = world_model(init_sequence + action_sequence)
+similarity = torch.cosine_similarity(continue_sequence, prompt)
 
 # v2: unified word model & control model
-init_sequence = uniform_model(noise, mode=world) # unified model will need some token generation restriction.
+init_sequence = uniform_model(
+    noise, mode=world
+)  # unified model will need some token generation restriction.
+action_sequence = uniform_model(init_sequence + prompt, mode=action)
+continue_sequence = uniform_model(init_sequence, action_sequence, mode=world)
+similarity = torch.cosine_similarity(continue_sequence, prompt)
 
-# v3: real world evaluators, no time traversal, but can change previous prompt (regret, backpropagate, (optionally) forget (maximize gradient) old (wrong) prediction  and learn (minimize gradient) actual (real) prediction)
-init_sequence = real_world(random_actions) # does this real world support time traversal?
+# v3: real world evaluators, could be no time traversal, but can change previous prompt (regret, backpropagate, (optionally) forget (maximize gradient) old (wrong) prediction and learn (minimize gradient) actual (real) prediction)
 
-if real_world.traversable: # @property
-    node_hash = real_world.commit() # usually this is automatically handled? no need to intentionally commit?
-    real_world.rollback(node_hash)
+init_sequence = real_world(
+    random_actions
+)  # does this real world support time traversal?
+
+if real_world.traversable:  # @property
+    node_hash = (
+        real_world.commit()
+    )  # usually this is automatically handled? no need to intentionally commit?
+    if not traverse_complete:
+        real_world.rollback(node_hash)
+else:
+    prompter_remember(current_prompt, current_outcome)
+    actor_remember(current_action, current_outcome)
+    actor_regret(prompt, current_action, target)
+    prompter_regret(prompt, target)# will change the prompt manufacturer
+    # prompt = prompt_manufacturer(target) -> action -> current_outcome
+    # delta_prompt, delta_action, delta_current_outcome -> closer than target
+
+    # you may add some noise when mismatch found.
+
+# prompt shall be crafted on both input tokens and target tokens.
 
 # use special token + target as prompt
 prompt = special_token + target
 
 # use thought token as prompt
-prompt = get_thought_tokens(special_token + target, seqlen) # thought tokens has to be "understood" by the model, so that we can know its intention (can we convert natural language prompt to thought tokens, aligned?)
+prompt = get_thought_tokens(
+    special_token + target, seqlen
+)  # thought tokens has to be "understood" by the model, so that we can know its intention (can we convert natural language prompt to thought tokens, aligned?)
+meaning = get_thought_meaning(prompt, token_space = english) # kind of like the machine using input method.
 
 # perform gradient descent based on cosine similarity, more similar result to target, more learning.
-# only perform descent on the most similar one.
+# only perform descent on the most similar one, or the most plausible way.
 # next time learn about the same pattern, we still select the most similar one, closing the gap.
 
 # a*: minimize distance from target to result + distance from source (init state) to result
 
 # the reward can also be related to "easyness". if the model is doing some easy stuff (predicted by the world model), then no intensive learning is performed. if what the model want is hard to realize in world, the model will learn hard to do it.
+reward = calculate_reward(computational_time, loss_delta)
+
+# q function shall be used with caution. it will have some weight against actual evaulation. only if it is trusted, we can give it some high weight value in order to reduce computation.
