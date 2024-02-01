52fc8410817c8bb73167857ae0b99ed58282897c
GitJournal <app@gitjournal.io>
2024-02-02 04:48:09 +0800

    update

--- active_documentation_helper/static/webterm.js
+++ active_documentation_helper/static/webterm.js
@@ -1,10 +1,17 @@
 "use strict";
 
-function dumpEventData(e){
+// when recording, emit action code, timestamp
+// generate the time to WAIT later, deduced from timestamp
+// you may not know the time for VIEW command. you may do that periodically
+// it might be unnecessary to use "TYPE" command.
+// paste action might be equivalent to "TYPE" command
+// REM statements can be manually added later.
+
+function dumpEventData(e) {
     let event_keys = Object.keys(e.__proto__);
     let event_data = {}
     for (let k of event_keys) {
-      event_data[k] = e[k];
+        event_data[k] = e[k];
     }
     return event_data;
 }
@@ -16,7 +23,8 @@ window.onload = () => {
     function connect() {
         socket = new WebSocket(`ws://${window.location.host}/ws`);
         socket.onmessage = e => terminal.update(JSON.parse(e.data));
-        socket.onclose = () => setTimeout(connect, 5000)
+        // socket.onclose = () => setTimeout(connect, 5000) // no reconnection!
+        socket.onclose = () => { console.log("Terminal closed."); alert("Terminal has been closed."); window.close(); }
     }
 
     connect();
@@ -25,8 +33,11 @@ window.onload = () => {
     element.onkeydown = e => {
         console.log("keydown", dumpEventData(e))
         let message = keyToMessage(e);
+        let action = keyToAction(e);
+        let timestamp = Date.now();
         console.log("message (keydown)", message)
         if (message !== null) {
+            console.log("<keydown> ACTION:", action, "MESSAGE:", message, "TIMESTAMP:", timestamp)
             socket.send(message);
             e.preventDefault();
             return false;
@@ -34,9 +45,16 @@ window.onload = () => {
     };
     element.onkeypress = e => {
         console.log("keypress", dumpEventData(e))
-        let message = keyToMessage(e);
+        // let action_and_message = keyToMessage(e);
+        let action = keyToAction(e)
+        // let action = action_and_message[0]
+        let message = keyToMessage(e)
+        // let message = action_and_message[1]
+        let timestamp = Date.now();
+        // let message = keyToMessage(e);
         console.log("message (keypress)", message)
         if (message !== null) {
+            console.log("<keypress> ACTION:", action, "MESSAGE:", message, "TIMESTAMP:", timestamp)
             socket.send(message);
         }
     };
@@ -48,7 +66,7 @@ class Terminal {
         this.width = width;
         this.height = height;
         this.screen = new Array(height);
-        this.cursor = {x: 0, y: 0};
+        this.cursor = { x: 0, y: 0 };
 
         const table = document.getElementById(id);
         for (let i = 0; i < height; i++) {
@@ -68,7 +86,7 @@ class Terminal {
         return document.getElementById(`char_${i}_${j}`);
     }
 
-    update({c: [cx, cy], lines}) {
+    update({ c: [cx, cy], lines }) {
         // Manual line feed hack. ``pyte.Screen`` does that lazily.
         if (cx == this.width) {
             cx = 0;
@@ -105,12 +123,12 @@ class Terminal {
     }
 
     updateCursor(x, y) {
-        this.cursor = {x, y};
+        this.cursor = { x, y };
         this.setStyle(this.charAt(y, x), "black", "white");
     }
 
     eraseCursor() {
-        const {x, y} = this.cursor;
+        const { x, y } = this.cursor;
         this.setStyle(this.charAt(y, x), "default", "default");
     }
 }
@@ -129,45 +147,104 @@ function keyToMessage(e) {
     console.assert(e.type === "keydown") // keydown is for special keys.
     let message = null;
     switch (e.which) {
-    case 8:
-        message = BACKSPACE; break;
-    case 9:
-        message = TAB; break;
-    case 13:  // Enter
-        message = "\n"; break;
-    case 27:
-        message = ESC; break;
-    case 33:  // PgUp
-        message = CSI + "5~"; break;
-    case 34:  // PgDn
-        message = CSI + "6~"; break;
-    case 35:  // End
-        message = CSI + "4~"; break;
-    case 36:  // Home
-        message = CSI + "1~"; break;
-    case 37:  // Left
-        message = CSI + "D"; break;
-    case 38:  // Up
-        message = e.metaKey ? ESC + "P" : CSI + "A"; break;
-    case 39:  // Right
-        message = CSI + "C"; break;
-    case 40:  // Down
-        message = e.metaKey ? ESC + "N" : CSI + "B"; break;
-    case 45:  // INS
-        message = CSI + "2~"; break;
-    case 46:  // DEL
-        message = CSI + "3~"; break;
-    default:
-        if (e.which >= 112 && e.which <= 123) {  // F1 -- F12
-            let number = e.which - 111;
-            message = F_N[number];
-        } else if (e.ctrlKey) {  // Ctrl + ...
-            if (e.keyCode >= 65 && e.keyCode <= 90) {     // keycode in A..Z
-                message = String.fromCharCode(e.keyCode - 65 + 1);
-            } else if (e.which >= 48 && e.which <= 57) {  // keycode in 0..9
-                message = CONTROL_N[e.which - 48];
+        case 8:
+            message = BACKSPACE; break;
+        case 9:
+            message = TAB; break;
+        case 13:  // Enter
+            message = "\n"; break;
+        case 27:
+            message = ESC; break;
+        case 33:  // PgUp
+            message = CSI + "5~"; break;
+        case 34:  // PgDn
+            message = CSI + "6~"; break;
+        case 35:  // End
+            message = CSI + "4~"; break;
+        case 36:  // Home
+            message = CSI + "1~"; break;
+        case 37:  // Left
+            message = CSI + "D"; break;
+        case 38:  // Up
+            message = e.metaKey ? ESC + "P" : CSI + "A"; break;
+        case 39:  // Right
+            message = CSI + "C"; break;
+        case 40:  // Down
+            message = e.metaKey ? ESC + "N" : CSI + "B"; break;
+        case 45:  // INS
+            message = CSI + "2~"; break;
+        case 46:  // DEL
+            message = CSI + "3~"; break;
+        default:
+            if (e.which >= 112 && e.which <= 123) {  // F1 -- F12
+                let number = e.which - 111;
+                message = F_N[number];
+            } else if (e.ctrlKey) {  // Ctrl + ...
+                if (e.keyCode >= 65 && e.keyCode <= 90) {     // keycode in A..Z
+                    message = String.fromCharCode(e.keyCode - 65 + 1);
+                } else if (e.which >= 48 && e.which <= 57) {  // keycode in 0..9
+                    message = CONTROL_N[e.which - 48];
+                }
             }
+    }
+
+    return message;
+}
+
+
+function keyToAction(e) {
+    if (e.type === "keypress") {
+        if (e.which !== 0 && e.charCode !== 0) {
+            return (e.which < 32)
+                ? null  // special symbol.
+                : 'TYPE';
         }
+
+        return null;    // special symbol.
+    }
+
+    console.assert(e.type === "keydown") // keydown is for special keys.
+    let message = null;
+    switch (e.which) {
+        case 8:
+            message = "BACKSPACE"; break;
+        case 9:
+            message = "TAB"; break;
+        case 13:  // Enter
+            message = "ENTER"; break;
+        case 27:
+            message = "ESC"; break;
+        case 33:  // PgUp
+            message = "PGUP"; break;
+        case 34:  // PgDn
+            message = "PGDN"; break;
+        case 35:  // End
+            message = "END"; break;
+        case 36:  // Home
+            message = "HOME"; break;
+        case 37:  // Left
+            message = "LEFT"; break;
+        case 38:  // Up
+            message = e.metaKey ? "META+UP" : "UP"; break;
+        case 39:  // Right
+            message = "RIGHT"; break;
+        case 40:  // Down
+            message = e.metaKey ? "META+DOWN" : "DOWN"; break;
+        case 45:  // INS
+            message = "INS"; break;
+        case 46:  // DEL
+            message = "DEL"; break;
+        default:
+            if (e.which >= 112 && e.which <= 123) {  // F1 -- F12
+                let number = e.which - 111;
+                message = "F" + number;
+            } else if (e.ctrlKey) {  // Ctrl + ...
+                if (e.keyCode >= 65 && e.keyCode <= 90) {     // keycode in A..Z
+                    message = "CTRL" + String.fromCharCode(e.keyCode);
+                } else if (e.which >= 48 && e.which <= 57) {  // keycode in 0..9
+                    message = "CTRL" + (e.which - 48);
+                }
+            }
     }
 
     return message;
