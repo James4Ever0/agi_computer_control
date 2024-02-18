b64325483cd73387254d423a63512c40ed6e97dd
GitJournal <app@gitjournal.io>
2024-02-18 13:52:08 +0800

    update

---
+++ active_documentation_helper/godlang_fuzzer.py
@@ -0,0 +1,120 @@
+import random
+
+special_command_list = [
+    "SPACE",
+    "BACKSPACE",
+    "TAB",
+    "ENTER",
+    "ESC",
+    "PGUP",
+    "PGDN",
+    "END",
+    "HOME",
+    "UP",
+    "META+UP",
+    "DOWN",
+    "META+DOWN",
+    "LEFT",
+    "RIGHT",
+    "INS",
+    "DEL",
+    "F1",
+    "F2",
+    "F3",
+    "F4",
+    "F5",
+    "F6",
+    "F7",
+    "F8",
+    "F9",
+    "F10",
+    "F11",
+    "F12",
+    "CTRL+A",
+    "CTRL+B",
+    "CTRL+C",
+    "CTRL+D",
+    "CTRL+E",
+    "CTRL+F",
+    "CTRL+G",
+    "CTRL+H",
+    "CTRL+I",
+    "CTRL+J",
+    "CTRL+K",
+    "CTRL+L",
+    "CTRL+M",
+    "CTRL+N",
+    "CTRL+O",
+    "CTRL+P",
+    "CTRL+Q",
+    "CTRL+R",
+    "CTRL+S",
+    "CTRL+T",
+    "CTRL+U",
+    "CTRL+V",
+    "CTRL+W",
+    "CTRL+X",
+    "CTRL+Y",
+    "CTRL+Z",
+    "CTRL+0",
+    "CTRL+1",
+    "CTRL+2",
+    "CTRL+3",
+    "CTRL+4",
+    "CTRL+5",
+    "CTRL+6",
+    "CTRL+7",
+    "CTRL+8",
+    "CTRL+9",
+]
+
+import string
+
+invisible_chars = [it for it in string.whitespace if it != "\n"]  # Invisible characters, including '\r'
+char_list = list(string.printable) + invisible_chars
+
+
+class CommandGenerator:
+    @staticmethod
+    def get_random_chars():
+        random_chars = "".join(random.sample(char_list, k=random.randint(1, 20)))
+        return random_chars
+
+    @staticmethod
+    def get_special_command():
+        special_command = random.choice(special_command_list)
+        return special_command
+
+    @staticmethod
+    def get_type_command():
+        special_command = CommandGenerator.get_special_command()
+        return f"TYPE {special_command}"
+
+    @staticmethod
+    def get_view_command():
+        return "VIEW"
+
+    @staticmethod
+    def get_wait_command():
+        wait_time = random.uniform(0.1, 10)
+        return f"WAIT {wait_time:.3f}"
+
+    @staticmethod
+    def get_reminder_command(reminder="Random actions"):
+        return f"REM {reminder}"
+
+
+command_generator_list = []
+
+
+def get_single_random_command():
+    command_generator_method_name = random.choice(
+        [it for it in dir(CommandGenerator) if it.startswith("get_")]
+    )
+    command: str = getattr(CommandGenerator,command_generator_method_name)()
+    return command
+
+if __name__ == "__main__":
+    for _ in range(100):
+        command = get_single_random_command()
+        print(command)
