9584236d3415238238f0f7a2b5f8397f9aee0a1b
lazero <lazero@lazero.com>
2024-07-18 14:03:22 +0800

    update

--- basic_interactive_program_emulation_and_image_with_docker_support/test_open_interpreter_local.sh
+++ basic_interactive_program_emulation_and_image_with_docker_support/test_open_interpreter_local.sh
@@ -3,12 +3,15 @@
 CONTAINER_NAME=openinterpreter_container
 SCRIPT_NAME=open_interpreter_tested.py
 
+PROMPT_PATH=/tmp/prompt.txt
+
 docker kill $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 
 docker run -d --rm --name $CONTAINER_NAME -e OPENAI_API_BASE=http://localhost:8101/v1 --network host openinterpreter tail -f /dev/null
 
 docker cp $SCRIPT_NAME $CONTAINER_NAME:/root/$SCRIPT_NAME
+docker cp $PROMPT_PATH $CONTAINER_NAME:$PROMPT_PATH
 
 docker exec -it $CONTAINER_NAME python3 /root/$SCRIPT_NAME
 
