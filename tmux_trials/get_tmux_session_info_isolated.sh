a03846d20afc18cd96e54c93ddee6d4c733c0477
lazero <lazero@lazero.com>
2024-08-06 01:48:07 +0800

    tmux python library init

--- tmux_trials/get_tmux_session_info_isolated.sh
+++ tmux_trials/get_tmux_session_info_isolated.sh
@@ -2,8 +2,9 @@
 # tmux list-commands # to get all commands
 # tmux list-commands | grep '\-F format'
 
-tmux list-sessions -F '[#{session_name}] bell: #{window_bell_flag} slient: #{window_silence_flag} active: #{window_activity_flag} socket: #{socket_path} size: #{window_width}x#{window_height} cursor at: x=#{cursor_x},y=#{cursor_y}'
+SESSION_NAME=agent_terminal
 
+tmux list-sessions -F '[#{session_name}] bell: #{window_bell_flag} slient: #{window_silence_flag} active: #{window_activity_flag} socket: #{socket_path} size: #{window_width}x#{window_height} cursor at: x=#{cursor_x},y=#{cursor_y}' | grep $SESSION_NAME
 # to run everything with different server, use `-L` or `-S` before command
 # tmux -L <server_name> new
 # tmux -S <server_file_path> new
