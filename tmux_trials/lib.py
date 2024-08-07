e4eb5676e8059fba525da7ae19096a4b4cade194
lazero <lazero@lazero.com>
2024-08-07 14:59:17 +0800

    session cursor insertion, minor bug fixes

--- tmux_trials/lib.py
+++ tmux_trials/lib.py
@@ -1,5 +1,5 @@
 from typing import Optional, Iterable
-import wcwidth
+from wcwidth import wcswidth
 import subprocess
 import os
 import traceback
@@ -12,13 +12,43 @@ TMUX = "tmux"
 TMUXP = "tmuxp"
 TMUX_WIDTH = 80
 TMUX_HEIGHT = 24
-ENCODING='utf-8'
+ENCODING = "utf-8"
+
+NEWLINE = "\n"
+NEWLINE_BYTES = NEWLINE.encode(ENCODING)
 
 CMDLIST_EXECUTE_TIMEOUT = 10
 
-CURSOR="<<<CURSOR>>>"
+CURSOR = "<<<CURSOR>>>"
+CURSOR_BYTES = CURSOR.encode(ENCODING)
 
 REQUIRED_BINARIES = (TMUX, TMUXP)
+
+
+def decode_bytes(_bytes: bytes, errors='ignore'):
+    ret = _bytes.decode(ENCODING, errors=errors)
+    return ret
+
+
+def insert_cursor(_bytes: bytes, x: int):
+    ret = b""
+    try:
+        line = decode_bytes(_bytes, errors='replace')
+        char_index = 0
+        for index, it in enumerate(line):
+            if char_index >= x:
+                ret = line[:index] + CURSOR + line[index:]
+                ret = ret.encode(ENCODING)
+                break
+            char_width = wcswidth(it)
+            char_index += char_width
+        if ret == b"":
+            ret = _bytes + CURSOR_BYTES
+    except UnicodeDecodeError:
+        print("[-] Failed to decode line while inserting cursor:", _bytes)
+        print("[*] Falling back to bytes insert mode")
+        ret = _bytes[:x] + CURSOR_BYTES + _bytes[x:]
+    return ret
 
 
 def assert_binary_existance(binaries: Iterable[str]):
@@ -28,16 +58,19 @@ def assert_binary_existance(binaries: Iterable[str]):
 
 assert_binary_existance(REQUIRED_BINARIES)
 
+
 def json_pretty_print(obj):
     ret = json.dumps(obj, indent=4, sort_keys=True, ensure_ascii=False)
     print(ret)
     return ret
 
-def warn_nonzero_exitcode(exitcode:int):
-    ret = exitcode==0
+
+def warn_nonzero_exitcode(exitcode: int):
+    ret = exitcode == 0
     if not ret:
-        print('[-] Process failed with exit code:', exitcode)
+        print("[-] Process failed with exit code:", exitcode)
     return ret
+
 
 class TmuxServer:
     def __init__(self, name: Optional[str] = None, reset: bool = True):
@@ -51,7 +84,6 @@ class TmuxServer:
 
         if reset:
             self.reset()
-
 
     def reset(self):
         exit_code = self.tmux_execute_command("kill-server")
@@ -92,11 +124,11 @@ class TmuxServer:
         ret = f"{self.prefix} {suffix}"
         return ret
 
-    def tmux_prepare_attach_command(self, name:str, view_only:bool): 
-        suffix="attach -t '{name}'"
+    def tmux_prepare_attach_command(self, name: str, view_only: bool):
+        suffix = f"attach -t '{name}'"
         if view_only:
-            suffix=f"{suffix} -r"
-        ret=self.tmux_prepare_command(suffix)
+            suffix = f"{suffix} -r"
+        ret = self.tmux_prepare_command(suffix)
         return ret
 
     def tmux_prepare_command_list(self, suffix_list: list[str]):
@@ -112,11 +144,11 @@ class TmuxServer:
         cmd = self.tmux_prepare_command(suffix)
         ret = self.execute_command(cmd)
         return ret
-    
-    def execute_command(self, cmd:str):
+
+    def execute_command(self, cmd: str):
         print("[*] Executing command:", cmd)
         exitcode = os.system(cmd)
-        ret=warn_nonzero_exitcode(exitcode)
+        ret = warn_nonzero_exitcode(exitcode)
         return ret
 
     def tmux_execute_command_list(self, suffix_list: list[str]):
@@ -125,20 +157,22 @@ class TmuxServer:
         if not ret:
             print("[-] Failed to exit command list in tmux")
         return ret
-    
-    def execute_command_list(self, cmd_list:list[str], timeout:Optional[float]=CMDLIST_EXECUTE_TIMEOUT):
+
+    def execute_command_list(
+        self, cmd_list: list[str], timeout: Optional[float] = CMDLIST_EXECUTE_TIMEOUT
+    ):
         print("[*] Executing command list:", *cmd_list)
         exitcode = subprocess.Popen(cmd_list).wait(timeout=timeout)
-        ret=warn_nonzero_exitcode(exitcode)
+        ret = warn_nonzero_exitcode(exitcode)
         return ret
 
-    def apply_manifest(self, manifest:dict, attach=False):
-        session_name = manifest['session_name']
+    def apply_manifest(self, manifest: dict, attach=False):
+        session_name = manifest["session_name"]
         print("[*] Tmuxp session name:", session_name)
         print("[*] Applying manifest")
-        with TemporaryFile('w+',suffix=".json") as f:
-            filepath= f.name
-            content= json_pretty_print(manifest)
+        with TemporaryFile("w+", suffix=".json") as f:
+            filepath = f.name
+            content = json_pretty_print(manifest)
             f.write(content)
             f.close()
             self.kill_session(session_name)
@@ -147,12 +181,12 @@ class TmuxServer:
             finally:
                 self.kill_session(session_name)
 
-    def tmuxp_load_from_filepath(self, filepath:str, attach:bool):
-        cmd_list = [TMUXP, 'load', '-L', self.name, '-f', filepath]
+    def tmuxp_load_from_filepath(self, filepath: str, attach: bool):
+        cmd_list = [TMUXP, "load", "-L", self.name, "-f", filepath]
         kwargs = {}
         if not attach:
             cmd_list.append("-d")
-            kwargs['timeout'] = None
+            kwargs["timeout"] = None
         ret = self.execute_command_list(cmd_list, **kwargs)
         if not ret:
             print("[-] Tmuxp manifest load failed")
@@ -194,12 +228,28 @@ class TmuxSession:
         self.set_option("window-size", "manual")
 
     def preview_bytes(self):
-        ret = self.server.tmux_get_command_output_bytes(f"capture-pane -t {self.name} -p")
+        ret = self.server.tmux_get_command_output_bytes(
+            f"capture-pane -t {self.name} -p"
+        )
         return ret
 
-    def preview(self,show_cursor=False):...
-        # info = self.get_info()
-        # if info is None: ...
+    def preview(self, show_cursor=False):
+        content_bytes = self.preview_bytes()
+        if show_cursor:
+            print("[*] Requested showing cursor")
+            info = self.get_info()
+            if info is None:
+                print("[-] Failed to fetch tmux session info")
+                return None
+            else:
+                x, y = info["cursor_x"], info["cursor_y"]
+                print("[*] Inserting cursor at: %d, %d" % (x, y))
+                content_byte_lines = content_bytes.splitlines()
+                cursor_line_bytes = content_byte_lines[y - 1]
+                content_byte_lines[y - 1] = insert_cursor(cursor_line_bytes, x - 1)
+                content_bytes = NEWLINE_BYTES.join(content_byte_lines)
+        ret = decode_bytes(content_bytes)
+        return ret
 
     def kill(self):
         self.server.kill_session(self.name)
@@ -207,30 +257,40 @@ class TmuxSession:
 
     def set_option(self, key: str, value: str):
         self.server.set_session_option(self.name, key, value)
-    
+
     def get_info(self):
-        list_session_format_template ="[#{session_name}] socket: #{socket_path} size: #{window_width}x#{window_height} cursor at: x=#{cursor_x},y=#{cursor_y} cursor flag: #{cursor_flag} cursor character: #{cursor_character}"
-        output_bytes = self.server.tmux_get_command_output_bytes(f"list-sessions -F '{list_session_format_template}'")
-        numeric_properties = ["window_width", "window_height", "cursor_x", "cursor_y", 'cursor_flag']
+        list_session_format_template = "[#{session_name}] socket: #{socket_path} size: #{window_width}x#{window_height} cursor at: x=#{cursor_x},y=#{cursor_y} cursor flag: #{cursor_flag} cursor character: #{cursor_character}"
+        output_bytes = self.server.tmux_get_command_output_bytes(
+            f"list-sessions -F '{list_session_format_template}'"
+        )
+        numeric_properties = [
+            "window_width",
+            "window_height",
+            "cursor_x",
+            "cursor_y",
+            "cursor_flag",
+        ]
         parse_format = list_session_format_template.replace("#{", "{")
         for it in numeric_properties:
-            parse_format = parse_format.replace(it, it+":d")
-        output = output_bytes.decode(ENCODING)
+            parse_format = parse_format.replace(it, it + ":d")
+        output = decode_bytes(output_bytes, errors='strict')
         data = parse.parse(parse_format, output)
         if data is not None:
             print("[+] Fetched info for session:", self.name)
-            ret = dict(data) # type: ignore
+            ret = dict(data)  # type: ignore
             json_pretty_print(ret)
             return ret
         else:
             print("[-] No info for session:", self.name)
+
     def create_viewer(self):
-        ret=TmuxSessionViewer(self)
+        ret = TmuxSessionViewer(self)
         return ret
 
     def view(self):
-        viewer=self.create_viewer()
+        viewer = self.create_viewer()
         viewer.view()
+
 
 class TmuxEnv:
     def __init__(self, session: TmuxSession):
@@ -240,7 +300,7 @@ class TmuxEnv:
     def send_keys(self, keys: list[str]):
         command_list = ["send-keys", "-t", self.session.name, *keys]
         self.server.tmux_execute_command_list(command_list)
-    
+
     def get_info(self):
         ret = self.session.get_info()
         return ret
@@ -249,23 +309,31 @@ class TmuxEnv:
         self.session.kill()
         del self
 
+
 class TmuxSessionViewer:
-    def __init__(self, session:TmuxSession):
-        self.session=session
+    def __init__(self, session: TmuxSession):
+        self.session = session
         self.server = session.server
-        self.name=session.name+"_viewer"
-        self.panes=[]
+        self.name = session.name + "_viewer"
+        self.panes = []
 
     @property
     def manifest(self):
-        ret={"session":{"name":self.name, "windows":[dict(name="viewer_window",layout="even-vertical",panes=self.panes]}}
+        ret = {
+            "session": {
+                "name": self.name,
+                "windows": [
+                    dict(name="viewer_window", layout="even-vertical", panes=self.panes)
+                ],
+            }
+        }
         return ret
 
-    def add_viewer_pane(self, pane_name:str, view_only=True):
-        cmd=self.server.tmux_prepare_attach_command(self.session.name, view_only)
-        self.add_cmd_pane(cmd, pane_name)
+    def add_viewer_pane(self, name: str, view_only=True):
+        cmd = self.server.tmux_prepare_attach_command(self.session.name, view_only)
+        self.add_cmd_pane(cmd, name)
 
-    def add_cmd_pane(self, cmd:str, pane_name:str):
+    def add_cmd_pane(self, cmd: str, name: str):
         self.panes.append(dict(shell_command=cmd, name=name))
 
     def view(self, view_only=False):
