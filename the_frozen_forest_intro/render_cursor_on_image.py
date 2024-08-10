e8a261528547d145368535270b138ae16ddc209a
lazero <lazero@lazero.com>
2024-08-10 16:04:01 +0800

    run formatter

--- the_frozen_forest_intro/render_cursor_on_image.py
+++ the_frozen_forest_intro/render_cursor_on_image.py
@@ -19,22 +19,27 @@ from PIL import Image
 
 def convert_cursor_in_red(cursor: ImageClass):
     red = (255, 0, 0)
-    cursor_red = Image.new('RGB', cursor.size, red)
+    cursor_red = Image.new("RGB", cursor.size, red)
 
     # Paste the red image onto the black and white image, using the black and white image as a mask
     cursor_red.paste(cursor_red, mask=cursor)
 
     return cursor_red
 
+
 def convert_to_grayscale(image: ImageClass) -> ImageClass:
     # Convert the image to grayscale
-    grayscale = image.convert('L')
+    grayscale = image.convert("L")
 
     # Convert the grayscale image back to RGB, while keeping the grayscale values in all three color channels
-    grayscale_rgb = Image.merge('RGB', (grayscale, grayscale, grayscale))
+    grayscale_rgb = Image.merge("RGB", (grayscale, grayscale, grayscale))
 
     return grayscale_rgb
-def place_cursor_to_screen(cursor:ImageClass, screen:ImageClass, position:tuple[int,int], red_on_grey:bool): 
+
+
+def place_cursor_to_screen(
+    cursor: ImageClass, screen: ImageClass, position: tuple[int, int], red_on_grey: bool
+):
     mask = cursor.copy()
     if red_on_grey:
         cursor = convert_cursor_in_red(cursor)
@@ -42,11 +47,11 @@ def place_cursor_to_screen(cursor:ImageClass, screen:ImageClass, position:tuple[
     ret = screen.copy()
     ret.paste(cursor, position, mask)
     return ret
-    
 
-def test(red_on_grey:bool):
+
+def test(red_on_grey: bool):
     position = (400, 400)
-    
+
     cursor_image_path = "cursor.png"
 
     screen_image_path = "paint.png"
@@ -54,14 +59,17 @@ def test(red_on_grey:bool):
     # screen_image_path = "baidu_7.png"
 
     cursor_image = Image.open(cursor_image_path)
-    screen_image = Image.open(screen_image_path).resize((992,647))
-    
+    screen_image = Image.open(screen_image_path).resize((992, 647))
+
     output_path = "image_with_cursor.png"
-    
-    output_image = place_cursor_to_screen(cursor_image, screen_image, position, red_on_grey)
-    
+
+    output_image = place_cursor_to_screen(
+        cursor_image, screen_image, position, red_on_grey
+    )
+
     print("[*] Saving to:", output_path)
     output_image.save(output_path)
+
 
 if __name__ == "__main__":
     # test(False)
