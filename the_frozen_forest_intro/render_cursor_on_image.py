cb1b22ba0c83307c013f307589989634913451ee
lazero <lazero@lazero.com>
2024-08-10 15:56:14 +0800

    add red on grey gui screenshot augmentation, refactored cursor placement code

--- the_frozen_forest_intro/render_cursor_on_image.py
+++ the_frozen_forest_intro/render_cursor_on_image.py
@@ -1,4 +1,9 @@
+from PIL.Image import Image as ImageClass
+
 from PIL import Image
+
+# typing issue reference:
+# https://github.com/microsoft/pylance-release/issues/5313
 
 # cursor_image = "mouse_cursor_white_16x16.png"
 
@@ -7,25 +12,57 @@ from PIL import Image
 
 # so, would you let me know where the cursur is at?
 
-cursor_image = "cursor.png"
-cur = Image.open(cursor_image)
-pos = (400, 400)
-pw = True
-
-if pw:
-    screen_image = "paint.png"
-else:
-    screen_image = "baidu_7.png"
-img = Image.open(screen_image)
-
 # client size: 992x647
 # size: 2232x1456
 # saved size: 2232x1456
-if pw:
-    new_img = img
-else:
-    new_img = img.resize((992, 647))  # 992x647, without scrollbar.
-# problem with scrollbar.
-new_img.paste(cur, pos, cur)  # misaligned.
 
-new_img.show()
+
+def convert_cursor_in_red(cursor: ImageClass):
+    red = (255, 0, 0)
+    cursor_red = Image.new('RGB', cursor.size, red)
+
+    # Paste the red image onto the black and white image, using the black and white image as a mask
+    cursor_red.paste(cursor_red, mask=cursor)
+
+    return cursor_red
+
+def convert_to_grayscale(image: ImageClass) -> ImageClass:
+    # Convert the image to grayscale
+    grayscale = image.convert('L')
+
+    # Convert the grayscale image back to RGB, while keeping the grayscale values in all three color channels
+    grayscale_rgb = Image.merge('RGB', (grayscale, grayscale, grayscale))
+
+    return grayscale_rgb
+def place_cursor_to_screen(cursor:ImageClass, screen:ImageClass, position:tuple[int,int], red_on_grey:bool): 
+    mask = cursor.copy()
+    if red_on_grey:
+        cursor = convert_cursor_in_red(cursor)
+        screen = convert_to_grayscale(screen)
+    ret = screen.copy()
+    ret.paste(cursor, position, mask)
+    return ret
+    
+
+def test(red_on_grey:bool):
+    position = (400, 400)
+    
+    cursor_image_path = "cursor.png"
+
+    screen_image_path = "paint.png"
+    # screen_image_path = "baidu_4.png"
+    # screen_image_path = "baidu_7.png"
+
+    cursor_image = Image.open(cursor_image_path)
+    screen_image = Image.open(screen_image_path).resize((992,647))
+    
+    output_path = "image_with_cursor.png"
+    
+    output_image = place_cursor_to_screen(cursor_image, screen_image, position, red_on_grey)
+    
+    print("[*] Saving to:", output_path)
+    output_image.save(output_path)
+
+if __name__ == "__main__":
+    # test(False)
+    test(True)
