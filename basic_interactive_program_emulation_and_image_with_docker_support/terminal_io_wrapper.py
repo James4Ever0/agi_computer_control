b6bb1c73963b3d9c018719fcce14730b629fa607
lazero <lazero@lazero.com>
2024-08-29 17:15:09 +0800

    implement terminal idle event emitter

--- basic_interactive_program_emulation_and_image_with_docker_support/terminal_io_wrapper.py
+++ basic_interactive_program_emulation_and_image_with_docker_support/terminal_io_wrapper.py
@@ -1,60 +1,71 @@
 # count for io stats
 from remote_socket import SocketClient
-import sys
-import tty
-tty.setcbreak(sys.stdin)
+import pexpect
+import time
+import threading
+import numpy as np
 
-# import signal
+TMUX_SESSION_NAME='test_tmux'
 
-# def signal_handler(signum, frame):
-#     print('Received SIGINT')
-#     pass
+def read_bytes_from_proc_and_update_stats(proc:pexpect.spawn, stats:dict):
+    count = 0
+    while proc.isalive():
+        one_byte = proc.read(1)
+        count += 1
+        stats['count'] = count
+        stats['bytes'] += one_byte
+        # print("[*] Received one byte at time:", time.time())
+        # print('[*] Total bytes received:', count)
 
-# # Set the signal handler for SIGINT (Ctrl+C)
-# signal.signal(signal.SIGINT, signal_handler)
-
-# this script is not usable, as special combos are not received at all.
-
-# import fcntl
-# import os
-# import select
-
-# # Make stdin non-blocking
-# fd = sys.stdin.fileno()
-# flags = fcntl.fcntl(fd, fcntl.F_GETFL)
-# fcntl.fcntl(fd, fcntl.F_SETFL, flags | os.O_NONBLOCK)
-
-# try:
-#     while True:
-#         # Use select to wait for input
-#         ready_to_read, _, _ = select.select([sys.stdin], [], [], 1.0)
-        
-#         if ready_to_read:
-#             raw_bytes = sys.stdin.buffer.read(1)  # Read one byte
-#             if not raw_bytes:  # EOF
-#                 break
-#             print(f'Received byte: {raw_bytes}')
-#         else:
-#             print('No input received, continuing...')
-
-# except KeyboardInterrupt:
-#     print('Exiting...')
-# finally:
-#     # Reset stdin to blocking mode if necessary
-#     fcntl.fcntl(fd, fcntl.F_SETFL, flags)
-
+def calculate_nth_average_speed(datalist:list, nth:int):
+    nth = min(len(datalist), nth)
+    diff = np.diff(datalist)
+    ret = sum(diff[:nth]) / nth
+    ret = float(-ret)
+    return ret
 
 def main():
-    # client = SocketClient()
-    total_count = 0
-    while True:
-        user_input = sys.stdin.buffer.read(1)
-        recv_len = len(user_input)
-        total_count += recv_len
-        # Process the raw bytes (for demonstration, we'll just print the length)
-        print(f'Received {recv_len} bytes.')
-        print(f"Total bytes received:", total_count)
-        # client.send(user_input)
+    cli = f"tmux attach -t {TMUX_SESSION_NAME}"
+    proc = pexpect.spawn(cli, timeout=None)
+    stats = {'count':0, 'bytes': b''}
+
+    read_thread = threading.Thread(target=read_bytes_from_proc_and_update_stats, args=(proc, stats), daemon=True)
+    read_thread.start()
+
+
+    datapoints = []
+
+    maxpoints = 10
+    terminal_idle = True
+    while proc.isalive():
+        time.sleep(1)
+        # print("-"*50)
+        # print('[*] Current time:', time.time())
+        # print('[*] Current IO stats:', stats)
+        count = stats.get('count')
+        read_bytes = stats.get('bytes')
+        stats['bytes'] = b'' # clear the clutter
+        datapoints.insert(0, count)
+        if len(datapoints) > maxpoints:
+            datapoints = datapoints[:maxpoints+1]
+        ten_seconds_avg = calculate_nth_average_speed(datapoints, 10)
+        five_seconds_avg = calculate_nth_average_speed(datapoints,5)
+        three_seconds_avg = calculate_nth_average_speed(datapoints,3)
+        one_second_avg = calculate_nth_average_speed(datapoints, 1)
+        # print('[*] Average bytes received in the last 10 seconds:', ten_seconds_avg)
+        # print('[*] Average bytes received in the last five seconds:', five_seconds_avg)
+        # print('[*] Average bytes received in the last three seconds:', three_seconds_avg)
+        # print('[*] Average bytes received in the last second:', one_second_avg)
+        if ten_seconds_avg > 0:
+        # if five_seconds_avg > 0:
+            if terminal_idle:
+                terminal_idle = False
+                print('[*] TerminalActive event fired!')
+        else:
+            if not terminal_idle:
+                terminal_idle = True
+                print('[*] TerminalIdle event fired!')
+    print('[*] Process exited with status code:', proc.status)
 
 if __name__ == '__main__':
     main()
---
+++ basic_interactive_program_emulation_and_image_with_docker_support/test_terminal_io_wrapper.sh
@@ -0,0 +1,5 @@
+tmux kill-session -t test_tmux
+tmux kill-session -t test_terminal_io_wrapper
+tmuxp load terminal_io_wrapper_test.yaml
+tmux kill-session -t test_tmux
+tmux kill-session -t test_terminal_io_wrapper
