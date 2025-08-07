9a69be16a42d3d05c3e549cfb2fc8a8e1c718087
Your Name <you@example.com>
2025-08-07 17:31:31 +0800

    add binary bytes tokenizer tests

--- binary_bytes_tokenizer/test_encode_binary_bytes.py
+++ binary_bytes_tokenizer/test_encode_binary_bytes.py
@@ -1,2 +1,64 @@
-# example in json: "\u001b[?2004h\u001b]0;root@50a6d006d95a: /\u0007root@50a6d006d95a:/# "
-binary_bytes = ...
+# try to use tiktoken to tokenize the above thing
+# tiktoken works
+
+
+def test_encode_with_tiktoken(input_string: str):
+    import tiktoken
+
+    enc = tiktoken.get_encoding("o200k_base")
+    try:
+        assert (
+            enc.decode_bytes(enc._encode_bytes(b"\xff\xff")) == b"\xff\xff"
+        )  # not working
+    except:  # let's not dig into this 'str' vs 'bytes' myth right now, and favor 'str' over 'bytes'
+        print("Bytes:", repr(b"\xff\xff"))
+        print("Encode:", repr(enc._encode_bytes(b"\xff\xff")))
+        print(
+            "Encode -> Decode result:",
+            repr(enc.decode_bytes(enc._encode_bytes(b"\xff\xff"))),
+        )
+    assert enc.decode(enc.encode(input_string)) == input_string
+
+    print("Input: %s" % repr(input_string))
+
+    encoded_tokens = enc.encode(input_string)
+    print("Encoded tokens: ")
+    print("", encoded_tokens)
+
+    for it in encoded_tokens:
+        print("Token: %s, Decoded: %s" % (it, repr(enc.decode([it]))))
+
+
+def test_encode_with_tokenizers(input_string: str):
+    import os
+
+    os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
+
+    from tokenizers import Tokenizer
+
+    tokenizer = Tokenizer.from_pretrained(
+        "openai/gpt-oss-20b"
+    )  # "bert-base-uncased" assertation failed. not lossless.
+    encoded_tokens = tokenizer.encode(input_string)
+
+    print("Input: %s" % repr(input_string))
+
+    print("Encoded tokens: ")
+    print("", encoded_tokens)
+
+    assert tokenizer.decode(ids=encoded_tokens.ids) == input_string
+
+
+def test():
+    # example in asciinema cast recording
+    input_string = (
+        "\u001b[?2004h\u001b]0;root@50a6d006d95a: /\u0007root@50a6d006d95a:/# "
+    )
+    print("Testing with tiktoken")
+    test_encode_with_tiktoken(input_string)
+    print("Testing with tokenizers")
+    test_encode_with_tokenizers(input_string)
+
+
+if __name__ == "__main__":
+    test()
