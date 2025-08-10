ed41aff030d0d82d1a4d0c99e63586f277278e33
Your Name <you@example.com>
2025-08-11 00:12:53 +0800

    add vimgolf challenge scraper code

--- scrape_vimgolf_challenges_and_solutions/parse_vimgolf_public_challenges.py
+++ scrape_vimgolf_challenges_and_solutions/parse_vimgolf_public_challenges.py
@@ -7,3 +7,14 @@ with open(html_filepath, 'r') as f:
 
 soup = BeautifulSoup(html_content, 'html.parser')
 
+for h5_elem in soup.find_all("h5", class_="challenge"):
+    # print("H5 element:", h5_elem)
+    a_href_elem = h5_elem.find("a")
+    href = a_href_elem['href']
+    title = a_href_elem.text
+    p_elem = h5_elem.find_next_sibling("p")
+    detail = p_elem.text
+    print()
+    print("Href:", href)
+    print("Title:", title)
+    print("Detail:", detail)
--- scrape_vimgolf_challenges_and_solutions/vimgolf_python_client_list_challenges.py
+++ scrape_vimgolf_challenges_and_solutions/vimgolf_python_client_list_challenges.py
@@ -1,7 +1,17 @@
 import subprocess
 import parse
 
-page_1_challenges_output = subprocess.check_output("vimgolf list 1")
+page_1_challenges_output = subprocess.check_output("vimgolf list 1".split()).decode()
 
-print("Page 1 challenges:")
-print(page_1_challenges_output)
+# print("Page 1 challenges:")
+
+# print(page_1_challenges_output)
+
+for line in page_1_challenges_output.splitlines():
+    if line.startswith("+"):
+        print("Parsing line:", line)
+        line = line.strip()
+        result = parse.parse('+{num} {title} - {entries_count} entries ({hash})', line)
+        print("Parse result:", result)
+    else:
+        print("Skipping line:", line)
